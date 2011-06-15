c4ca38dd680f0bb8d4276c1aa116b724c4a43cf4
Daniel Lidstrom <dlidstrom@gmail.com>
2011-06-15 20:34:49 +0200

    Added installer for Raven

---
+++ SnittListan/Installers/RavenInstaller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Castle.MicroKernel;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Raven.Client;
+using Raven.Client.Embedded;
+
+namespace SnittListan.Installers
+{
+	public class RavenInstaller : IWindsorInstaller
+	{
+		public void Install(IWindsorContainer container, IConfigurationStore store)
+		{
+			container.Register(
+					Component.For<IDocumentStore>().Instance(CreateDocumentStore()).LifeStyle.Singleton,
+					Component.For<IDocumentSession>().UsingFactoryMethod(GetDocumentSession).LifeStyle.PerWebRequest
+				);
+		}
+
+		private IDocumentStore CreateDocumentStore()
+		{
+			var store = new EmbeddableDocumentStore
+			{
+				DataDirectory = AppDomain.CurrentDomain.GetData("DataDirectory").ToString()
+			}.Initialize();
+
+			return store;
+		}
+
+		private static IDocumentSession GetDocumentSession(IKernel kernel)
+		{
+			var store = kernel.Resolve<IDocumentStore>();
+			return store.OpenSession();
+		}
+	}
+}
