d59ed72203c1b40f0c748a0793c2278306ef67fb
Daniel Lidstrom <dlidstrom@gmail.com>
2011-06-22 21:26:42 +0200

    Windsor installer for services

---
+++ SnittListan/Installers/ServicesInstaller.cs
@@ -0,0 +1,19 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace SnittListan.Installers
+{
+	public class ServicesInstaller : IWindsorInstaller
+	{
+		public void Install(IWindsorContainer container, IConfigurationStore store)
+		{
+			container.Register(
+				AllTypes
+					.FromThisAssembly()
+					.Where(Component.IsInNamespace("SnittListan.Services"))
+					.WithService.DefaultInterface()
+					.Configure(c => c.LifeStyle.Transient));
+		}
+	}
+}
