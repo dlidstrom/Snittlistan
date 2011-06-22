e838f91d95f0eb370bbfa6d2415ff525f5fcf3a7
Daniel Lidstrom <dlidstrom@gmail.com>
2011-06-22 18:40:46 +0200

    Minor

--- SnittListan/Global.asax.cs
+++ SnittListan/Global.asax.cs
@@ -9,9 +9,6 @@ using SnittListan.IoC;
 
 namespace SnittListan
 {
-	// Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-	// visit http://go.microsoft.com/?LinkId=9394801
-
 	public class MvcApplication : HttpApplication
 	{
 		private static readonly ILog logger = LogManager.GetCurrentClassLogger();
