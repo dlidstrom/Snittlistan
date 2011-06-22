8b3658a7a442fb8850ba74a846fbf5f32a4141e3
Daniel Lidstrom <dlidstrom@gmail.com>
2011-06-22 21:31:33 +0200

    Added test for services installer
    Factored out installer test helpers

--- SnittListan.Test/ControllersInstallerTest.cs
+++ SnittListan.Test/ControllersInstallerTest.cs
@@ -24,8 +24,8 @@ namespace SnittListan.Test
 		[Fact]
 		public void AllControllersImplementIController()
 		{
-			var allHandlers = GetAllHandlers(container);
-			var controllerHandlers = GetHandlersFor(typeof(IController), container);
+			var allHandlers = InstallerTestHelper.GetAllHandlers(container);
+			var controllerHandlers = InstallerTestHelper.GetHandlersFor(typeof(IController), container);
 			Assert.NotEmpty(allHandlers);
 			Assert.Equal(allHandlers, controllerHandlers);
 		}
@@ -36,31 +36,31 @@ namespace SnittListan.Test
 			// Is<TType> is a helper extension method from Windsor
 			// which behaves like 'is' keyword in C# but at a Type, not instance level
 
-			var allControllers = GetPublicClassesFromApplicationAssembly(c => c.Is<IController>());
-			var registeredControllers = GetImplementationTypesFor(typeof(IController), container);
+			var allControllers = InstallerTestHelper.GetPublicClassesFromApplicationAssembly(c => c.Is<IController>());
+			var registeredControllers = InstallerTestHelper.GetImplementationTypesFor(typeof(IController), container);
 			Assert.Equal(allControllers, registeredControllers);
 		}
 
 		[Fact]
 		public void AllAndOnlyControllersHaveControllerSuffix()
 		{
-			var allControllers = GetPublicClassesFromApplicationAssembly(c => c.Name.EndsWith("Controller"));
-			var registeredControllers = GetImplementationTypesFor(typeof(IController), container);
+			var allControllers = InstallerTestHelper.GetPublicClassesFromApplicationAssembly(c => c.Name.EndsWith("Controller"));
+			var registeredControllers = InstallerTestHelper.GetImplementationTypesFor(typeof(IController), container);
 			Assert.Equal(allControllers, registeredControllers);
 		}
 
 		[Fact]
 		public void AllAndOnlyControllersLiveInControllersNamespace()
 		{
-			var allControllers = GetPublicClassesFromApplicationAssembly(c => c.Namespace.Contains("Controllers"));
-			var registeredControllers = GetImplementationTypesFor(typeof(IController), container);
+			var allControllers = InstallerTestHelper.GetPublicClassesFromApplicationAssembly(c => c.Namespace.Contains("Controllers"));
+			var registeredControllers = InstallerTestHelper.GetImplementationTypesFor(typeof(IController), container);
 			Assert.Equal(allControllers, registeredControllers);
 		}
 
 		[Fact]
 		public void AllControllersAreTransient()
 		{
-			var nonTransientControllers = GetHandlersFor(typeof(IController), container)
+			var nonTransientControllers = InstallerTestHelper.GetHandlersFor(typeof(IController), container)
 				.Where(c => c.ComponentModel.LifestyleType != LifestyleType.Transient)
 				.ToArray();
 			Assert.Empty(nonTransientControllers);
@@ -69,38 +69,10 @@ namespace SnittListan.Test
 		[Fact]
 		public void AllControllersExposeThemselvesAsService()
 		{
-			var controllersWithWrongName = GetHandlersFor(typeof(IController), container)
+			var controllersWithWrongName = InstallerTestHelper.GetHandlersFor(typeof(IController), container)
 				.Where(c => c.Service != c.ComponentModel.Implementation)
 				.ToArray();
 			Assert.Empty(controllersWithWrongName);
-		}
-
-		private static IHandler[] GetAllHandlers(IWindsorContainer container)
-		{
-			return GetHandlersFor(typeof(object), container);
-		}
-
-		private static IHandler[] GetHandlersFor(Type type, IWindsorContainer container)
-		{
-			return container.Kernel.GetAssignableHandlers(type);
-		}
-
-		private static Type[] GetImplementationTypesFor(Type type, IWindsorContainer container)
-		{
-			return GetHandlersFor(type, container)
-				.Select(h => h.ComponentModel.Implementation)
-				.OrderBy(t => t.Name)
-				.ToArray();
-		}
-
-		private static Type[] GetPublicClassesFromApplicationAssembly(Predicate<Type> where)
-		{
-			return typeof(HomeController).Assembly.GetExportedTypes()
-				.Where(t => t.IsClass)
-				.Where(t => t.IsAbstract == false)
-				.Where(where.Invoke)
-				.OrderBy(t => t.Name)
-				.ToArray();
 		}
 	}
 }
---
+++ SnittListan.Test/InstallerTestHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using Castle.MicroKernel;
+using Castle.Windsor;
+using SnittListan.Controllers;
+
+namespace SnittListan.Test
+{
+	public static class InstallerTestHelper
+	{
+		public static IHandler[] GetAllHandlers(IWindsorContainer container)
+		{
+			return GetHandlersFor(typeof(object), container);
+		}
+
+		public static IHandler[] GetHandlersFor(Type type, IWindsorContainer container)
+		{
+			return container.Kernel.GetAssignableHandlers(type);
+		}
+
+		public static Type[] GetImplementationTypesFor(Type type, IWindsorContainer container)
+		{
+			return GetHandlersFor(type, container)
+				.Select(h => h.ComponentModel.Implementation)
+				.OrderBy(t => t.Name)
+				.ToArray();
+		}
+
+		public static Type[] GetPublicClassesFromApplicationAssembly(Predicate<Type> where)
+		{
+			return typeof(HomeController).Assembly.GetExportedTypes()
+				.Where(t => t.IsClass)
+				.Where(t => t.IsAbstract == false)
+				.Where(where.Invoke)
+				.OrderBy(t => t.Name)
+				.ToArray();
+		}
+	}
+}
