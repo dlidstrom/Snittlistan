51d5a18ac0cd7b368824d79a02aea393122483d5
Daniel Lidstrom <dlidstrom@gmail.com>
2011-06-22 22:55:40 +0200

    Added handler for new user registrations

---
+++ SnittListan.Test/SendRegistrationEmailHandlerTest.cs
@@ -0,0 +1,16 @@
+﻿using Moq;
+using SnittListan.Services;
+using Xunit;
+
+namespace SnittListan.Test
+{
+	public class SendRegistrationEmailHandlerTest
+	{
+		[Fact]
+		public void ShouldSendMail()
+		{
+			var handler = new SendRegistrationEmailHandler(Mock.Of<IEmailService>());
+			Assert.True(false);
+		}
+	}
+}
