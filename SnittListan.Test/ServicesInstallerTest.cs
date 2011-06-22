c45c0d03ea01ab9163601bea2ecc8d2c87580247
Daniel Lidstrom <dlidstrom@gmail.com>
2011-06-22 21:31:33 +0200

    Added test for services installer
    Factored out installer test helpers

---
+++ SnittListan.Test/ServicesInstallerTest.cs
@@ -0,0 +1,23 @@
+﻿using Castle.Windsor;
+using SnittListan.Installers;
+using SnittListan.Services;
+using Xunit;
+
+namespace SnittListan.Test
+{
+	public class ServicesInstallerTest
+	{
+		private readonly IWindsorContainer container;
+
+		public ServicesInstallerTest()
+		{
+			container = new WindsorContainer().Install(new ServicesInstaller());
+		}
+
+		[Fact]
+		public void InstallsFormsAuthenticationService()
+		{
+			Assert.NotNull(container.Resolve<IFormsAuthenticationService>());
+		}
+	}
+}
