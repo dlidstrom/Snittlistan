1df71f63642e05c68c185b061f00f9b60f074537
Daniel Lidstrom <dlidstrom@gmail.com>
2011-06-30 22:48:12 +0200

    Handlers installer and tests

---
+++ SnittListan/Installers/HandlersInstaller.cs
@@ -0,0 +1,20 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using SnittListan.Events;
+using SnittListan.Handlers;
+
+namespace SnittListan.Installers
+{
+	public class HandlersInstaller : IWindsorInstaller
+	{
+		public void Install(IWindsorContainer container, IConfigurationStore store)
+		{
+			container.Register(
+				AllTypes.FromThisAssembly()
+				.BasedOn(typeof(IHandle<>))
+				.If(Component.IsInSameNamespaceAs<SendRegistrationEmailHandler>())
+				.Configure(c => c.LifeStyle.Transient));
+		}
+	}
+}
