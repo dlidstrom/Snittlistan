67c11235459faea534130ea8f0bb2500cf190a52
Daniel Lidstrom <dlidstrom@gmail.com>
2011-06-30 22:48:12 +0200

    Handlers installer and tests

---
+++ SnittListan.Test/HandlersInstallerTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Castle.Core;
+using Castle.Windsor;
+using SnittListan.Events;
+using SnittListan.Handlers;
+using SnittListan.Installers;
+using Xunit;
+using Castle.MicroKernel.Registration;
+using SnittListan.Services;
+using Moq;
+
+namespace SnittListan.Test
+{
+	public class HandlersInstallerTest
+	{
+		private readonly IWindsorContainer container;
+
+		public HandlersInstallerTest()
+		{
+			container = new WindsorContainer().Install(new HandlersInstaller());
+		}
+
+		[Fact]
+		public void InstallsHandlerForNewUserCreatedEvent()
+		{
+			var handler = InstallerTestHelper.GetHandlersFor(typeof(IHandle<NewUserCreatedEvent>), container);
+			Assert.Equal(1, handler.Length);
+		}
+
+		[Fact]
+		public void HandlersAreTransient()
+		{
+			var nonTransientControllers = InstallerTestHelper.GetHandlersFor(typeof(IHandle<>), container)
+				.Where(c => c.ComponentModel.LifestyleType != LifestyleType.Transient)
+				.ToArray();
+			Assert.Empty(nonTransientControllers);
+		}
+	}
+}
