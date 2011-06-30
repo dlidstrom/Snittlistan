4f34344903de4ff631567197b5ef9538aeba57e3
Daniel Lidstrom <dlidstrom@gmail.com>
2011-06-30 22:53:11 +0200

    Email server default settings come from Web.config
    Overloaded constructor can be used for tests

--- SnittListan.Test/EmailServiceTest.cs
+++ SnittListan.Test/EmailServiceTest.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Xunit;
+using SnittListan.Services;
 
 namespace SnittListan.Test
 {
 	public class EmailServiceTest
 	{
-		[Fact]
+		[Fact(Skip="Configure before executing")]
 		public void ShouldDeliverMail()
 		{
-			// configure service to write to some directory, then verify results
-			Assert.True(false);
+			// this test actually sends a mail
+			string host = "mail.snittlistan.se";
+			int port = 1045;
+			string username = "elmah@snittlistan.se";
+			string password = "____";
+			var service = new EmailService(host, port, username, password, new string[] { "dlidstrom@gmail.com" });
+
+			service.SendMail("dlidstrom@gmail.com", "Test", "Message body");
 		}
 	}
 }
