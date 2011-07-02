e76e0ab90633d485b9696410e440351388979947
Daniel Lidstrom <dlidstrom@gmail.com>
2011-07-02 20:55:16 +0200

    Implemented some route shortcuts
    Routes can now be tested for correctness.

---
+++ SnittListan.Test/RoutesTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MvcContrib.TestHelper;
+using Xunit;
+using SnittListan.Controllers;
+using System.Web.Routing;
+
+namespace SnittListan.Test
+{
+	public class RoutesTest : IDisposable
+	{
+		public RoutesTest()
+		{
+			new RouteConfigurator(RouteTable.Routes).Configure();
+		}
+
+		public void Dispose()
+		{
+			RouteTable.Routes.Clear();
+		}
+
+		[Fact]
+		public void DefaultRoute()
+		{
+			"~/".ShouldMapTo<HomeController>(c => c.Index());
+		}
+
+		[Fact]
+		public void LowerCaseRoutes()
+		{
+			"~/account/register".ShouldMapTo<AccountController>(c => c.Register());
+		}
+
+		[Fact]
+		public void Shortcuts()
+		{
+			"~/register".ShouldMapTo<AccountController>(c => c.Register());
+			"~/logon".ShouldMapTo<AccountController>(c => c.LogOn());
+			"~/about".ShouldMapTo<HomeController>(c => c.About());
+		}
+	}
+}
