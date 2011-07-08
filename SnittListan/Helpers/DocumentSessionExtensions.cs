f2a386d3db1b1cc8311976c4e926c5f806df0b03
Daniel Lidstrom <dlidstrom@gmail.com>
2011-07-08 22:30:07 +0200

    Forgot about extension method

--- SnittListan/Helpers/DocumentSessionExtensions.cs
+++ SnittListan/Helpers/DocumentSessionExtensions.cs
@@ -10,5 +10,10 @@ namespace SnittListan.Helpers
 		{
 			return sess.Query<User>().Where(u => u.Email == email);
 		}
+
+		public static IQueryable<User> FindUserByActivationKey(this IDocumentSession sess, string key)
+		{
+			return sess.Query<User>().Where(u => u.ActivationKey == key);
+		}
 	}
 }
