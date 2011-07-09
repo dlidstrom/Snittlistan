d495d60b3373150e2feef429e4d58f2fb6c2cb87
Daniel Lidstrom <dlidstrom@gmail.com>
2011-07-09 12:50:17 +0200

    Workaround for potential problem with RavenDB 397
    It seems there needs to be something in the store before
    it is possible to query.

--- SnittListan.Test/DbTest.cs
+++ SnittListan.Test/DbTest.cs
@@ -1,9 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Moq;
 using Raven.Client;
 using Raven.Client.Embedded;
+using SnittListan.Models;
 
 namespace SnittListan.Test
 {
@@ -18,6 +20,10 @@ namespace SnittListan.Test
 				RunInMemory = true
 			}.Initialize();
 			Session = store.OpenSession();
+			// this is a workaround
+			// to be able to query, at least one document must be stored
+			Session.Store(new User("Daniel", "Lidström", "someone@somedomain.com", "some password"));
+			Session.SaveChanges();
 		}
 
 		public IDocumentSession Session { get; private set; }
@@ -25,6 +31,14 @@ namespace SnittListan.Test
 		public void Dispose()
 		{
 			Session.Dispose();
+		}
+
+		public UrlHelper CreateUrlHelper()
+		{
+			var context = Mock.Of<HttpContextBase>();
+			var routes = new RouteCollection();
+			new RouteConfigurator(routes).Configure();
+			return new UrlHelper(new RequestContext(Mock.Get(context).Object, new RouteData()), routes);
 		}
 	}
 }
