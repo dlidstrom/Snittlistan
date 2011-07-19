f490a08ec27914611f6096b9fff430d639b8ce09
Daniel Lidstrom <dlidstrom@gmail.com>
2011-07-19 20:31:20 +0200

    Show teams and final result

--- SnittListan/Infrastructure/AutoMapperProfiles/MatchProfile.cs
+++ SnittListan/Infrastructure/AutoMapperProfiles/MatchProfile.cs
@@ -9,7 +9,9 @@ namespace SnittListan.Infrastructure
 		protected override void Configure()
 		{
 			Mapper.CreateMap<Match, MatchViewModel>()
-				.ForMember(x => x.Date, o => o.MapFrom(y => y.Date.ToShortDateString()));
+				.ForMember(x => x.Date, o => o.MapFrom(y => y.Date.ToShortDateString()))
+				.ForMember(x => x.Results, o => o.MapFrom(y => y.FormattedLaneScore()))
+				.ForMember(x => x.Teams, o => o.MapFrom(y => string.Format("{0}-{1}", y.HomeTeam, y.OppTeam)));
 		}
 	}
 }
