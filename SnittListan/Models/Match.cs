becbaa8e0d503933ee933932bc98f8f5fbee3d58
Daniel Lidstrom <dlidstrom@gmail.com>
2011-07-19 17:15:05 +0200

    Simple model for bowling games

---
+++ SnittListan/Models/Match.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SnittListan.Models
+{
+	public class Match
+	{
+		public string Id { get; private set; }
+		public string Place { get; set; }
+		public DateTime Date { get; set; }
+
+		public List<Serie> Series { get; set; }
+	}
+}
