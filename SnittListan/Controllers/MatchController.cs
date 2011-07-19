1c50eead6ee5eac4141557358d4df4a75b7cc0af
Daniel Lidstrom <dlidstrom@gmail.com>
2011-07-19 19:24:16 +0200

    Pin scores and lane scores

--- SnittListan/Controllers/MatchController.cs
+++ SnittListan/Controllers/MatchController.cs
@@ -11,76 +11,11 @@ namespace SnittListan.Controllers
 {
     public class MatchController : Controller
     {
-		private List<Match> matches;
+		private MatchRepository repo;
 
 		public MatchController()
 		{
-			var match = new Match
-			{
-				Date = DateTime.ParseExact("2011-03-26", "yyyy-MM-dd", CultureInfo.InvariantCulture),
-				Place = "Mälarhallen",
-				Series = new List<Serie>
-				{
-					new Serie
-					{
-						Games = new List<Game>
-						{
-							new Game(1, "M. Axelsson", 202, 1),
-							new Game(1, "C. Liedholm", 218, 1),
-							new Game(2, "K. Persson", 172, 0),
-							new Game(2, "P. Sjöberg", 220, 0),
-							new Game(3, "K. Jansson", 166, 0),
-							new Game(3, "H. Norbeck", 194, 0),
-							new Game(4, "L. Öberg", 222, 1),
-							new Game(4, "T. Jensen", 204, 1)
-						}
-					},
-					new Serie
-					{
-						Games = new List<Game>
-						{
-							new Game(1, "L. Öberg", 182, 1),
-							new Game(1, "T. Jensen", 211, 1),
-							new Game(2, "K. Jansson", 208, 1),
-							new Game(2, "H. Norbeck", 227, 1),
-							new Game(3, "K. Persson", 194, 0),
-							new Game(3, "P. Sjöberg", 195, 0),
-							new Game(4, "M. Axelsson", 206, 0),
-							new Game(4, "C. Liedholm", 150, 0)
-						}
-					},
-					new Serie
-					{
-						Games = new List<Game>
-						{
-							new Game(1, "K. Persson", 174, 0),
-							new Game(1, "P. Sjöberg", 182, 0),
-							new Game(2, "M. Axelsson", 214, 1),
-							new Game(2, "C. Liedholm", 176, 1),
-							new Game(3, "L. Öberg", 168, 0),
-							new Game(3, "T. Jensen", 199, 0),
-							new Game(4, "K. Jansson", 180, 0),
-							new Game(4, "H. Norbeck", 212, 0),
-						}
-					},
-					new Serie
-					{
-						Games = new List<Game>
-						{
-							new Game(1, "K. Jansson", 189, 0),
-							new Game(1, "H. Norbeck", 181, 0),
-							new Game(2, "L. Öberg", 227, 0),
-							new Game(2, "T. Jensen", 180, 0),
-							new Game(3, "M. Axelsson", 223, 1),
-							new Game(3, "C. Liedholm", 191, 1),
-							new Game(4, "T. Gurell", 159, 0),
-							new Game(4, "P. Sjöberg", 190, 0),
-						}
-					}
-				}
-			};
-
-			matches = new Match[] { match }.ToList();
+			repo = new MatchRepository();
 		}
 
 		/// <summary>
@@ -89,7 +24,7 @@ namespace SnittListan.Controllers
 		/// <returns></returns>
         public ActionResult Index()
         {
-            return View(matches.MapTo<MatchViewModel>());
+            return View(repo.LoadAll(0, 25).MapTo<MatchViewModel>());
         }
 
 		/// <summary>
@@ -97,9 +32,9 @@ namespace SnittListan.Controllers
 		/// </summary>
 		/// <param name="id"></param>
 		/// <returns></returns>
-        public ActionResult Details(string id)
+        public ActionResult Details(int id)
         {
-			return View(matches.Where(m => m.Id == id).Single().MapTo<MatchViewModel>());
+			return View(repo.Query().Where(m => m.Id == id).Single().MapTo<MatchViewModel>());
         }
 
 		/// <summary>
@@ -135,9 +70,9 @@ namespace SnittListan.Controllers
  		/// </summary>
  		/// <param name="id"></param>
  		/// <returns></returns>
-        public ActionResult Edit(string id)
+        public ActionResult Edit(int id)
         {
-            return View(matches.Where(m => m.Id == id).Single());
+            return View(repo.Query().Where(m => m.Id == id).Single());
         }
 
 		/// <summary>
@@ -164,9 +99,9 @@ namespace SnittListan.Controllers
  		/// </summary>
  		/// <param name="id"></param>
  		/// <returns></returns>
-        public ActionResult Delete(string id)
+        public ActionResult Delete(int id)
         {
-			return View(matches.Where(m => m.Id == id).Single());
+			return View(repo.Query().Where(m => m.Id == id).Single());
         }
 
 		/// <summary>
