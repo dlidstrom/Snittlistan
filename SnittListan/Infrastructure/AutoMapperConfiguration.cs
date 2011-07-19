d4759402cc5b9556ccafd205942517538e6b8346
Daniel Lidstrom <dlidstrom@gmail.com>
2011-07-19 17:12:36 +0200

    AutoMapper configuration and extensions

---
+++ SnittListan/Infrastructure/AutoMapperConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using SnittListan.Models;
+using SnittListan.ViewModels;
+
+namespace SnittListan.Infrastructure
+{
+	public static class AutoMapperConfiguration
+	{
+		public static void Configure()
+		{
+			Mapper.CreateMap<Match, MatchViewModel>();
+		}
+	}
+}
