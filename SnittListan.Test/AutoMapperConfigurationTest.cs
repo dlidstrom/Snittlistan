2b0d16f6eef1411bd293a12318905d96cfc2df63
Daniel Lidstrom <dlidstrom@gmail.com>
2011-07-19 17:09:13 +0200

    Automapper test

---
+++ SnittListan.Test/AutoMapperConfigurationTest.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using SnittListan.Infrastructure;
+using Xunit;
+
+namespace SnittListan.Test
+{
+	public class AutoMapperConfigurationTest
+	{
+		[Fact]
+		public void VerifyConfiguration()
+		{
+			AutoMapperConfiguration.Configure();
+			Mapper.AssertConfigurationIsValid();
+		}
+	}
+}
