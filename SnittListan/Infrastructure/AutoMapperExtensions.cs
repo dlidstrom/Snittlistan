56ddecb4f8db9905976c799c8e8cbbfb579693e7
Daniel Lidstrom <dlidstrom@gmail.com>
2011-07-19 17:12:36 +0200

    AutoMapper configuration and extensions

---
+++ SnittListan/Infrastructure/AutoMapperExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using AutoMapper;
+
+namespace SnittListan.Infrastructure
+{
+	public static class AutoMapperExtensions
+	{
+		public static List<TResult> MapTo<TResult>(this IEnumerable self)
+		{
+			if (self == null)
+				throw new ArgumentNullException("self");
+
+			return (List<TResult>)Mapper.Map(self, self.GetType(), typeof(List<TResult>));
+		}
+	}
+}
---
+++ SnittListan/ViewModels/MatchViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SnittListan.ViewModels
+{
+	public class MatchViewModel
+	{
+		public string Id { get; set; }
+		public string Place { get; set; }
+		public string Date { get; set; }
+	}
+}
