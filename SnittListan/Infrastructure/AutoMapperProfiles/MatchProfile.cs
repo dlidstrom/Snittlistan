32f12fde387a51e0c8940a7170d7499bc33eb107
Daniel Lidstrom <dlidstrom@gmail.com>
2011-08-08 21:15:10 +0200

    Display series for match

--- SnittListan/Infrastructure/AutoMapperProfiles/MatchProfile.cs
+++ SnittListan/Infrastructure/AutoMapperProfiles/MatchProfile.cs
@@ -12,7 +12,10 @@ namespace SnittListan.Infrastructure
 			Mapper.CreateMap<Match, MatchViewModel>()
 				.ForMember(x => x.Date, o => o.MapFrom(y => y.Date.ToString(Thread.CurrentThread.CurrentCulture.DateTimeFormat.ShortDatePattern, Thread.CurrentThread.CurrentCulture)))
 				.ForMember(x => x.Results, o => o.MapFrom(y => y.FormattedLaneScore()))
-				.ForMember(x => x.Teams, o => o.MapFrom(y => string.Format("{0}-{1}", y.HomeTeam, y.OppTeam)));
+				.ForMember(x => x.Teams, o => o.MapFrom(y => string.Format("{0}-{1}", y.HomeTeam, y.OppTeam)))
+				.ForMember(x => x.Games, o => o.MapFrom(y => y.Games));
+
+			Mapper.CreateMap<Game, MatchViewModel.Game>();
 		}
 	}
 }
--- SnittListan/ViewModels/MatchViewModel.cs
+++ SnittListan/ViewModels/MatchViewModel.cs
@@ -7,5 +7,13 @@
 		public string Date { get; set; }
 		public string Results { get; set; }
 		public string Teams { get; set; }
+		public Game[] Games { get; set; }
+
+		public class Game
+		{
+			public string Player { get; set; }
+			public int PinScore { get; set; }
+			public int LaneScore { get; set; }
+		}
 	}
 }
