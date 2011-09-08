1da176d57349bd0a5b971755047726ffcadde7de
Daniel Lidstrom <dlidstrom@gmail.com>
2011-09-08 08:14:52 +0200

    Renamed SnittListan to Snittlistan

--- Snittlistan/Properties/AssemblyInfo.cs
+++ Snittlistan/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SnittListan")]
+[assembly: AssemblyTitle("Snittlistan")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("SnittListan")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("PDL")]
+[assembly: AssemblyProduct("Snittlistan")]
+[assembly: AssemblyCopyright("Copyright © Daniel Lidström 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
