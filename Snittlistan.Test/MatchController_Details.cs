8afb047c27980103dc3bb7d37704ab996abc3e5d
Daniel Lidstrom <dlidstrom@gmail.com>
2011-09-12 22:15:23 +0200

    Added specifications for Details and Index

---
+++ Snittlistan.Test/MatchController_Details.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MvcContrib.TestHelper;
+using Snittlistan.Controllers;
+using Snittlistan.Models;
+using Snittlistan.ViewModels;
+using Xunit;
+
+namespace Snittlistan.Test
+{
+	public class MatchController_Details : DbTest
+	{
+		[Fact]
+		public void ShouldViewMatch()
+		{
+			// Arrange
+			Session.Store(new Match("P1", DateTime.Now, 1, new Team("Home1", 4), new Team("Away1", 7)) { Id = 1 });
+			Session.Store(new Match("P2", DateTime.Now, 2, new Team("Home2", 5), new Team("Away2", 8)) { Id = 2 });
+			Session.Store(new Match("P3", DateTime.Now, 3, new Team("Home3", 6), new Team("Away3", 9)) { Id = 3 });
+			WaitForNonStaleResults<Match>();
+			var controller = new MatchController(Session);
+
+			// Act
+			var model1 = controller.Details(1).Model as MatchViewModel;
+			var model2 = controller.Details(1).Model as MatchViewModel;
+			var model3 = controller.Details(1).Model as MatchViewModel;
+
+			// Assert
+			model1.Match.Id.ShouldBe(1);
+			model2.Match.Id.ShouldBe(2);
+			model3.Match.Id.ShouldBe(3);
+		}
+	}
+}
---
+++ Snittlistan.Test/MatchController_Index.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MvcContrib.TestHelper;
+using Snittlistan.Controllers;
+using Snittlistan.Models;
+using Snittlistan.ViewModels;
+using Xunit;
+
+namespace Snittlistan.Test
+{
+	public class MatchController_Index : DbTest
+	{
+		[Fact]
+		public void ShouldViewIndex()
+		{
+			// Arrange
+			var controller = new MatchController(Session);
+
+			// Act
+			var result = controller.Index();
+
+			// Assert
+			result.AssertViewRendered().ForView(string.Empty);
+		}
+
+		[Fact]
+		public void ShouldListMatches()
+		{
+			// Arrange
+			var now = DateTime.Now;
+			Session.Store(new Match("P1", now, 1, new Team("Home", 1), new Team("Away", 2)));
+			Session.Store(new Match("P2", now.AddDays(1), 2, new Team("Home2", 3), new Team("Away2", 4)));
+			Session.SaveChanges();
+			WaitForNonStaleResults<Match>();
+
+			// Act
+			var controller = new MatchController(Session);
+			var result = controller.Index().Model as IEnumerable<MatchViewModel>;
+
+			// Assert
+			var matches = result.ToArray();
+			matches.Length.ShouldBe(2);
+			matches[0].Match.Place.ShouldBe("P");
+			matches[1].Match.Place.ShouldBe("P2");
+		}
+	}
+}
