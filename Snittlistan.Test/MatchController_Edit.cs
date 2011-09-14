7afebf9e9854c5605f644a6742b35d7b181838dc
Daniel Lidstrom <dlidstrom@gmail.com>
2011-09-14 08:47:15 +0200

    Added specification for editing match details

---
+++ Snittlistan.Test/MatchController_Edit.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using MvcContrib.TestHelper;
+using Snittlistan.Models;
+using Snittlistan.Controllers;
+using Snittlistan.ViewModels;
+
+namespace Snittlistan.Test
+{
+	public class MatchController_Edit : DbTest
+	{
+		[Fact]
+		public void CanEditMatch()
+		{
+			// Arrange
+			var then = DateTime.Now.AddDays(-1);
+			Match originalMatch = new Match("Place", then, 1, new Team("Home", 13), new Team("Away", 6));
+			Session.Store(originalMatch);
+			Session.SaveChanges();
+			WaitForNonStaleResults<Match>();
+
+			// Act
+			var controller = new MatchController(Session);
+			var now = DateTime.Now;
+			var result = controller.Edit(new MatchViewModel.MatchDetails
+			{
+				Id = originalMatch.Id,
+				Place = "NewPlace",
+				Date = now,
+				BitsMatchId = 2
+			});
+
+			// Assert
+			result.AssertActionRedirect().ToAction("Details").WithParameter("id", originalMatch.Id);
+			var match = Session.Load<Match>(originalMatch.Id);
+			match.Place.ShouldBe("NewPlace");
+			match.Date.ShouldBe(now);
+			match.BitsMatchId.ShouldBe(2);
+		}
+
+		[Fact]
+		public void CannotEditNonExistingMatch()
+		{
+			Assert.True(false, "Not implemented yet");
+		}
+
+		[Fact]
+		public void CanEditTeam()
+		{
+			Assert.True(false, "Not implemented yet");
+		}
+	}
+}
