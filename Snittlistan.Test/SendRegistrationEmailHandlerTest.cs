c8753996e2b58a4a7f3d8877c3c244cf2d497c87
Daniel Lidstrom <dlidstrom@gmail.com>
2011-09-19 23:25:13 +0200

    Actually run the test too

--- Snittlistan.Test/SendRegistrationEmailHandlerTest.cs
+++ Snittlistan.Test/SendRegistrationEmailHandlerTest.cs
@@ -4,13 +4,16 @@ using Snittlistan.Events;
 using Snittlistan.Handlers;
 using Snittlistan.Models;
 using Snittlistan.Services;
+using Xunit;
 
 namespace Snittlistan.Test
 {
 	public class SendRegistrationEmailHandlerTest
 	{
+		[Fact]
 		public void ShouldSendMail()
 		{
+			// Arrange
 			var service = Mock.Of<IEmailService>();
 			var recipient = It.Is<string>(s => s == "e@d.com");
 			var subject = It.Is<string>(s => s == "subject");
@@ -18,11 +21,15 @@ namespace Snittlistan.Test
 			Mock.Get(service)
 				.Setup(s => s.SendMail(recipient, subject, It.IsAny<string>()))
 				.Callback(() => mailSent = true);
+
+			// Act
 			var handler = new SendRegistrationEmailHandler(service);
 			handler.Handle(new NewUserCreatedEvent
 			{
 				User = new User("F", "L", "e@d.com", "some pwd")
 			});
+
+			// Assert
 			mailSent.ShouldBe(true);
 		}
 	}
