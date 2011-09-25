2362e6934301c7b4962d6323341841cc521a0846
Daniel Lidstrom <dlidstrom@gmail.com>
2011-09-25 12:39:04 +0200

    Improved documentation

--- Snittlistan/ViewModels/TeamViewModel.cs
+++ Snittlistan/ViewModels/TeamViewModel.cs
@@ -4,8 +4,14 @@ using System.Web.Mvc;
 
 namespace Snittlistan.ViewModels
 {
+	/// <summary>
+	/// Represents a team.
+	/// </summary>
 	public class TeamViewModel
 	{
+		/// <summary>
+		/// Initializes a new instance of the TeamViewModel class.
+		/// </summary>
 		public TeamViewModel()
 		{
 			Series = new List<Serie> { new Serie() };
@@ -14,26 +20,50 @@ namespace Snittlistan.ViewModels
 		[HiddenInput]
 		public int Id { get; set; }
 
+		/// <summary>
+		/// Gets or sets the name.
+		/// </summary>
 		[Required, Display(Name = "Namn")]
 		public string Name { get; set; }
 
+		/// <summary>
+		/// Gets or sets the score.
+		/// </summary>
 		[Required, Display(Name = "Banpoäng"), Range(0, 20)]
 		public int Score { get; set; }
 
+		/// <summary>
+		/// Gets or sets the series.
+		/// </summary>
 		public List<Serie> Series { get; set; }
 
+		/// <summary>
+		/// Represents a serie in a match.
+		/// </summary>
 		public class Serie
 		{
+			/// <summary>
+			/// Initializes a new instance of the Serie class.
+			/// </summary>
 			public Serie()
 			{
 				Tables = new List<Table> { new Table() };
 			}
 
+			/// <summary>
+			/// Gets or sets the tables.
+			/// </summary>
 			public List<Table> Tables { get; set; }
 		}
 
+		/// <summary>
+		/// Represents a table in a serie.
+		/// </summary>
 		public class Table
 		{
+			/// <summary>
+			/// Initializes a new instance of the Table class.
+			/// </summary>
 			public Table()
 			{
 				FirstGame = new Game();
@@ -46,12 +76,25 @@ namespace Snittlistan.ViewModels
 			[Display(Name = "Banpoäng")]
 			public int Score { get; set; }
 
+			/// <summary>
+			/// Gets or sets the first game.
+			/// </summary>
 			public Game FirstGame { get; set; }
+
+			/// <summary>
+			/// Gets or sets the second game.
+			/// </summary>
 			public Game SecondGame { get; set; }
 		}
 		
+		/// <summary>
+		/// Represents a game in a table.
+		/// </summary>
 		public class Game
 		{
+			/// <summary>
+			/// Initializes a new instance of the Game class.
+			/// </summary>
 			public Game()
 			{
 				Player = string.Empty;
