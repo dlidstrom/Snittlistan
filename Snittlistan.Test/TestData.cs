63bcc5a2cc6e3ebe8419a0882d0f8dc9854e4e9a
Daniel Lidstrom <dlidstrom@gmail.com>
2011-10-09 15:17:46 +0200

    Refactored some duplicated code

--- Snittlistan.Test/TestData.cs
+++ Snittlistan.Test/TestData.cs
@@ -2,6 +2,7 @@
 {
 	using System;
 	using System.Collections.Generic;
+	using MvcContrib.TestHelper;
 	using Snittlistan.Models;
 
 	public class TestData
@@ -140,6 +141,23 @@
 			match.AwayTeam.Series = series;
 
 			return match;
+		}
+
+		public static void VerifyMatch(Match match)
+		{
+			match.AwayTeam.Name.ShouldBe("Fredrikshof IF");
+			match.AwayTeam.PinsForPlayer("Peter Sjöberg").ShouldBe(787);
+			match.AwayTeam.Series.Count.ShouldBe(4);
+			match.AwayTeam.Pins().ShouldBe(6216);
+			match.AwayTeam.PinsFor(1).ShouldBe(1598);
+			match.AwayTeam.PinsFor(2).ShouldBe(1573);
+			match.AwayTeam.PinsFor(3).ShouldBe(1505);
+			match.AwayTeam.PinsFor(4).ShouldBe(1540);
+			match.AwayTeam.ScoreFor(1).ShouldBe(2);
+			match.AwayTeam.ScoreFor(2).ShouldBe(2);
+			match.AwayTeam.ScoreFor(3).ShouldBe(1);
+			match.AwayTeam.ScoreFor(4).ShouldBe(1);
+			match.AwayTeam.Score.ShouldBe(6);
 		}
 	}
 }
