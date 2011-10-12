9eb467e41076530859af0d4517332d747d870837
Daniel Lidstrom <dlidstrom@gmail.com>
2011-10-12 21:36:09 +0200

    Run Raven in different modes in prod and test
    Embedded in production, and standalone server when developing.

--- Snittlistan/Installers/RavenInstaller.cs
+++ Snittlistan/Installers/RavenInstaller.cs
@@ -4,12 +4,21 @@ using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
 using Raven.Client;
+using Raven.Client.Document;
 using Raven.Client.Embedded;
+using Raven.Client.Indexes;
+using Snittlistan.Infrastructure.Indexes;
 
 namespace Snittlistan.Installers
 {
 	public class RavenInstaller : IWindsorInstaller
 	{
+#if DEBUG
+		private bool runningInDebug = true;
+#else
+		private static bool runningInDebug = false;
+#endif
+
 		public void Install(IWindsorContainer container, IConfigurationStore store)
 		{
 			container.Register(
@@ -25,15 +34,16 @@ namespace Snittlistan.Installers
 
 		private IDocumentStore CreateDocumentStore()
 		{
-			var store = new EmbeddableDocumentStore
-			{
-				DataDirectory = AppDomain.CurrentDomain.GetData("DataDirectory").ToString(),
-#if DEBUG
-				UseEmbeddedHttpServer = true
-#endif
-			}.Initialize();
+			// run with server when debugging, and embedded in production
+			IDocumentStore store = null;
+			if (runningInDebug)
+				store = new DocumentStore { ConnectionStringName = "RavenDB" };
+			else
+				store = new EmbeddableDocumentStore { DataDirectory = AppDomain.CurrentDomain.GetData("DataDirectory").ToString() };
 
+			store.Initialize();
 			store.Conventions.IdentityPartsSeparator = "-";
+			IndexCreation.CreateIndexes(typeof(Matches_PlayerStats).Assembly, store);
 			return store;
 		}
 	}
