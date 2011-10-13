d8bfb596324681340e5c18b05b08442317992400
Daniel Lidstrom <dlidstrom@gmail.com>
2011-10-13 19:41:00 +0200

    Can now show players stats on home page

--- Snittlistan/Controllers/HomeController.cs
+++ Snittlistan/Controllers/HomeController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Raven.Client;
+using Snittlistan.Infrastructure.Indexes;
 
 namespace Snittlistan.Controllers
 {
 	public class HomeController : AbstractController
 	{
+		public HomeController(IDocumentSession session)
+			: base(session)
+		{ }
+
 		public ActionResult Index()
 		{
-			return View();
+			var stats = Session.Query<Matches_PlayerStats.Results, Matches_PlayerStats>()
+				.OrderByDescending(s => s.TotalPins)
+				.ToList();
+
+			return View(stats);
 		}
 
 		public ActionResult About()
