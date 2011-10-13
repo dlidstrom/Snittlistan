0d283b7b91602ca39288a9ed3bdf911a3e4249cc
Daniel Lidstrom <dlidstrom@gmail.com>
2011-10-13 19:39:41 +0200

    Match should be serializable with Newtonsoft.Json

---
+++ Snittlistan.Test/SerializationTest.cs
@@ -0,0 +1,22 @@
+﻿namespace Snittlistan.Test
+{
+	using Newtonsoft.Json;
+	using Snittlistan.Models;
+	using Xunit;
+
+	public class SerializationTest
+	{
+		[Fact]
+		public void CanSerializeMatch()
+		{
+			// Arrange
+			string output = JsonConvert.SerializeObject(DbSeed.CreateMatch());
+
+			// Act
+			var match = JsonConvert.DeserializeObject<Match>(output);
+
+			// Assert
+			TestData.VerifyMatch(match);
+		}
+	}
+}
