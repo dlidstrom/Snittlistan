b75a94e48cd12896c7d346dbd4ebf4cc96a1f9e9
Daniel Lidstrom <dlidstrom@gmail.com>
2011-10-17 15:02:15 +0200

    Changed match registration to pair-wise
    This is consistent with how match reports are written. The old method of
    serie-wise means a lot of page-flipping.

--- Snittlistan/Infrastructure/AutoMapper/Profiles/TeamViewModelConverter.cs
+++ Snittlistan/Infrastructure/AutoMapper/Profiles/TeamViewModelConverter.cs
@@ -14,12 +14,36 @@ namespace Snittlistan.Infrastructure.AutoMapper.Profiles
 				vm.Name,
 				vm.Score,
 				new List<Serie>
-				{
-					vm.Serie1.MapTo<Serie>(),
-					vm.Serie2.MapTo<Serie>(),
-					vm.Serie3.MapTo<Serie>(),
-					vm.Serie4.MapTo<Serie>()
-				});
+			    {
+					new Serie(new List<Table>
+					{
+						vm.Pair1.Serie1.MapTo<Table>(),
+						vm.Pair2.Serie1.MapTo<Table>(),
+						vm.Pair3.Serie1.MapTo<Table>(),
+						vm.Pair4.Serie1.MapTo<Table>()
+					}),
+					new Serie(new List<Table>
+					{
+						vm.Pair3.Serie2.MapTo<Table>(),
+						vm.Pair4.Serie2.MapTo<Table>(),
+						vm.Pair1.Serie2.MapTo<Table>(),
+						vm.Pair2.Serie2.MapTo<Table>()
+					}),
+					new Serie(new List<Table>
+					{
+						vm.Pair4.Serie3.MapTo<Table>(),
+						vm.Pair3.Serie3.MapTo<Table>(),
+						vm.Pair2.Serie3.MapTo<Table>(),
+						vm.Pair1.Serie3.MapTo<Table>()
+					}),
+					new Serie(new List<Table>
+					{
+						vm.Pair2.Serie4.MapTo<Table>(),
+						vm.Pair1.Serie4.MapTo<Table>(),
+						vm.Pair4.Serie4.MapTo<Table>(),
+						vm.Pair3.Serie4.MapTo<Table>()
+					}),
+			    });
 		}
 	}
 }
