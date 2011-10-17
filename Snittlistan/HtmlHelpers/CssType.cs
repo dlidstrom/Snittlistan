39dd088249f12dccbfad737c3aed2eb114f46235
Daniel Lidstrom <dlidstrom@gmail.com>
2011-10-17 16:57:21 +0200

    Use jQuery from aspnetcdn

---
+++ Snittlistan/HtmlHelpers/CssType.cs
@@ -0,0 +1,10 @@
+﻿namespace Snittlistan.HtmlHelpers
+{
+	public enum CssType
+	{
+		/// <summary>
+		/// Blitzer jQuery-UI theme.
+		/// </summary>
+		Blitzer
+	}
+}
---
+++ Snittlistan/HtmlHelpers/ScriptType.cs
@@ -0,0 +1,30 @@
+﻿namespace Snittlistan.HtmlHelpers
+{
+	public enum ScriptType
+	{
+		/// <summary>
+		/// Main jQuery library.
+		/// </summary>
+		JQuery,
+
+		/// <summary>
+		/// Validation library of jQuery.
+		/// </summary>
+		JQueryValidate,
+
+		/// <summary>
+		/// ASP.NET jQuery Validate Unobtrusive.
+		/// </summary>
+		JQueryValidateUnobtrusive,
+
+		/// <summary>
+		/// User interface library.
+		/// </summary>
+		JQueryUI,
+
+		/// <summary>
+		/// Modernizer library.
+		/// </summary>
+		Modernizr
+	}
+}
