be65df31383427ba5b6530e9e5e5257c894328de
Daniel Lidstrom <dlidstrom@gmail.com>
2011-10-17 11:36:42 +0200

    Improved documentation

--- Snittlistan/Events/DomainEvent.cs
+++ Snittlistan/Events/DomainEvent.cs
@@ -1,19 +1,40 @@
-﻿using System;
-using Castle.Windsor;
-using Snittlistan.Handlers;
-
-namespace Snittlistan.Events
+﻿namespace Snittlistan.Events
 {
+	using System;
+	using Castle.Windsor;
+	using Snittlistan.Handlers;
+
+	/// <summary>
+	/// Used to raise events within the domain model.
+	/// </summary>
 	public static class DomainEvent
 	{
+		/// <summary>
+		/// Initializes static members of the DomainEvent class.
+		/// </summary>
 		static DomainEvent()
 		{
 			ReturnContainer = DefaultReturnContainer;
 		}
 
+		/// <summary>
+		/// Gets or sets a function that supplies the container.
+		/// Can be used to set a container for tests.
+		/// </summary>
 		public static Func<IWindsorContainer> ReturnContainer { get; set; }
+
+		/// <summary>
+		/// Gets or sets an action used to raise events.
+		/// Used for testing purposes.
+		/// </summary>
 		public static Action<IDomainEvent> RaiseAction { get; set; }
 
+		/// <summary>
+		/// Raises a domain event. Default action is to resolve all handlers
+		/// and let them handle the event.
+		/// </summary>
+		/// <typeparam name="TEvent">Type of event.</typeparam>
+		/// <param name="event">Event to raise.</param>
 		public static void Raise<TEvent>(TEvent @event) where TEvent : IDomainEvent
 		{
 			if (RaiseAction != null)
@@ -32,6 +53,11 @@ namespace Snittlistan.Events
 			}
 		}
 
+		/// <summary>
+		/// Used for testing purposes.
+		/// </summary>
+		/// <param name="raiseAction">Custom raise action.</param>
+		/// <returns>Domain reset event to go back to default behaviour.</returns>
 		public static DomainEventReset TestWith(Action<IDomainEvent> raiseAction)
 		{
 			RaiseAction = raiseAction;
@@ -39,11 +65,23 @@ namespace Snittlistan.Events
 			return new DomainEventReset();
 		}
 
+		/// <summary>
+		/// Resets the raise action to default behaviour.
+		/// </summary>
 		public static void Reset()
 		{
 			RaiseAction = null;
 		}
 
+		/// <summary>
+		/// Used for testing purposes. Use within a using-scope:
+		/// <code>
+		/// using (DomainEvent.Disable())
+		/// { ... }
+		/// </code>
+		/// No events will be raised within the using-scope.
+		/// </summary>
+		/// <returns>Event reset to default behaviour.</returns>
 		public static DomainEventReset Disable()
 		{
 			RaiseAction = e => { };
@@ -51,6 +89,10 @@ namespace Snittlistan.Events
 			return new DomainEventReset();
 		}
 
+		/// <summary>
+		/// Returns the default container.
+		/// </summary>
+		/// <returns>The global container.</returns>
 		private static IWindsorContainer DefaultReturnContainer()
 		{
 			return MvcApplication.Container;
--- Snittlistan/Events/NewUserCreatedEvent.cs
+++ Snittlistan/Events/NewUserCreatedEvent.cs
@@ -1,9 +1,15 @@
-﻿using Snittlistan.Models;
-
-namespace Snittlistan.Events
+﻿namespace Snittlistan.Events
 {
+	using Snittlistan.Models;
+
+	/// <summary>
+	/// Raised when a new user is created.
+	/// </summary>
 	public class NewUserCreatedEvent : IDomainEvent
 	{
+		/// <summary>
+		/// Gets or sets the user.
+		/// </summary>
 		public User User { get; set; }
 	}
 }
