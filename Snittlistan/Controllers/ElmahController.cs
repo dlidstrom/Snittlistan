d6fcd2272c0f965641e596bf2fa60c1e903ce309
Daniel Lidstrom <dlidstrom@gmail.com>
2011-10-25 23:51:58 +0200

    Elmah is now protected

---
+++ Snittlistan/Controllers/ElmahController.cs
@@ -0,0 +1,14 @@
+﻿namespace Snittlistan.Controllers
+{
+	using System.Web.Mvc;
+	using Snittlistan.Helpers.Results;
+
+	[Authorize]
+	public class ElmahController : Controller
+    {
+        public ActionResult Index(string type)
+        {
+			return new ElmahResult(type);
+        }
+    }
+}
