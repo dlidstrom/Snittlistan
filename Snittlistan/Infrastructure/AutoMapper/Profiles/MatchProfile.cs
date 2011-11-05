660682678ba86d004a8ff6664de182aada00e983
Daniel Lidstrom <dlidstrom@gmail.com>
2011-11-05 17:05:42 +0100

    Show a summary view of series

--- Snittlistan/Infrastructure/AutoMapper/Profiles/MatchProfile.cs
+++ Snittlistan/Infrastructure/AutoMapper/Profiles/MatchProfile.cs
@@ -6,28 +6,32 @@ using Snittlistan.ViewModels;
 
 namespace Snittlistan.Infrastructure.AutoMapper.Profiles
 {
-	public class MatchProfile : Profile
-	{
-		protected override void Configure()
-		{
-			// model -> viewmodel
-			Mapper.CreateMap<Match, MatchViewModel.MatchDetails>();
-			Mapper.CreateMap<Team, TeamSummaryViewModel>();
-			Mapper.CreateMap<Team, TeamViewModel>()
-				.ForMember(vm => vm.Pair1, o => o.ResolveUsing(new PairResolver { Pair = 0 }))
-				.ForMember(vm => vm.Pair2, o => o.ResolveUsing(new PairResolver { Pair = 1 }))
-				.ForMember(vm => vm.Pair3, o => o.ResolveUsing(new PairResolver { Pair = 2 }))
-				.ForMember(vm => vm.Pair4, o => o.ResolveUsing(new PairResolver { Pair = 3 }));
-			Mapper.CreateMap<Table, TeamViewModel.Serie>();
-			Mapper.CreateMap<Game, TeamViewModel.Game>();
+    public class MatchProfile : Profile
+    {
+        protected override void Configure()
+        {
+            // model -> viewmodel
+            Mapper.CreateMap<Match, MatchViewModel.MatchDetails>();
+            Mapper.CreateMap<Team, TeamSummaryViewModel>();
+            Mapper.CreateMap<Serie, TeamSummaryViewModel.Serie>();
+            Mapper.CreateMap<Table, TeamSummaryViewModel.Table>()
+                .ForMember(vm => vm.Total, o => o.ResolveUsing(m => m.Game1.Pins + m.Game2.Pins));
+            Mapper.CreateMap<Game, TeamSummaryViewModel.Game>();
+            Mapper.CreateMap<Team, TeamViewModel>()
+                .ForMember(vm => vm.Pair1, o => o.ResolveUsing(new PairResolver { Pair = 0 }))
+                .ForMember(vm => vm.Pair2, o => o.ResolveUsing(new PairResolver { Pair = 1 }))
+                .ForMember(vm => vm.Pair3, o => o.ResolveUsing(new PairResolver { Pair = 2 }))
+                .ForMember(vm => vm.Pair4, o => o.ResolveUsing(new PairResolver { Pair = 3 }));
+            Mapper.CreateMap<Table, TeamViewModel.Serie>();
+            Mapper.CreateMap<Game, TeamViewModel.Game>();
 
-			// viewmodel -> model
-			Mapper.CreateMap<TeamViewModel, HomeTeamFactory>()
-				.ConvertUsing<TeamViewModelConverter<HomeTeamFactory>>();
-			Mapper.CreateMap<TeamViewModel, AwayTeamFactory>()
-				.ConvertUsing<TeamViewModelConverter<AwayTeamFactory>>();
-			Mapper.CreateMap<TeamViewModel.Serie, Table>();
-			Mapper.CreateMap<TeamViewModel.Game, Game>();
-		}
-	}
+            // viewmodel -> model
+            Mapper.CreateMap<TeamViewModel, HomeTeamFactory>()
+                .ConvertUsing<TeamViewModelConverter<HomeTeamFactory>>();
+            Mapper.CreateMap<TeamViewModel, AwayTeamFactory>()
+                .ConvertUsing<TeamViewModelConverter<AwayTeamFactory>>();
+            Mapper.CreateMap<TeamViewModel.Serie, Table>();
+            Mapper.CreateMap<TeamViewModel.Game, Game>();
+        }
+    }
 }
