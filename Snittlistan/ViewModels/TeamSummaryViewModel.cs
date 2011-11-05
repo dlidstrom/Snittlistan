d31f54e27f0d17cadaa4713cd26121fe23a82487
Daniel Lidstrom <dlidstrom@gmail.com>
2011-11-05 17:05:42 +0100

    Show a summary view of series

--- Snittlistan/ViewModels/TeamSummaryViewModel.cs
+++ Snittlistan/ViewModels/TeamSummaryViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Snittlistan.ViewModels
 {
@@ -9,5 +10,26 @@ namespace Snittlistan.ViewModels
 
 		[Display(Name = "Banpoäng")]
 		public int Score { get; set; }
+
+        public List<Serie> Series { get; set; }
+
+        public class Serie
+        {
+            public List<Table> Tables { get; set; }
+        }
+
+        public class Table
+        {
+            public int Score { get; set; }
+            public int Total { get; set; }
+            public Game Game1 { get; set; }
+            public Game Game2 { get; set; }
+        }
+
+        public class Game
+        {
+            public string Player { get; set; }
+            public int Pins { get; set; }
+        }
 	}
 }
