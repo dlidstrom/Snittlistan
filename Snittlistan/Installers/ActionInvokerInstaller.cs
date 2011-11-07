f7bda8f0073e9179f8ac1988df8a3965690d73c5
Daniel Lidstrom <dlidstrom@gmail.com>
2011-11-07 16:00:28 +0100

    Injecting action invoker used for IoC in filters
    Use to inject services into filters. This removes
    one need for Service Locator.

---
+++ Snittlistan/Installers/ActionInvokerInstaller.cs
@@ -0,0 +1,19 @@
+﻿namespace Snittlistan.Installers
+{
+    using System.Web.Mvc;
+    using Castle.MicroKernel.Registration;
+    using Castle.MicroKernel.SubSystems.Configuration;
+    using Castle.Windsor;
+    using Snittlistan.Infrastructure;
+
+    public class ActionInvokerInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(
+                Component.For<IActionInvoker>()
+                .ImplementedBy<InjectingActionInvoker>()
+                .LifestyleTransient());
+        }
+    }
+}
