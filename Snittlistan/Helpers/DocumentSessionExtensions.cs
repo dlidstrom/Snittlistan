c71cc23646f430c6ccd1401b3344a2a1fe0a821e
Daniel Lidstrom <dlidstrom@gmail.com>
2011-11-08 17:44:03 +0100

    Simplify extension methods

--- Snittlistan/Helpers/DocumentSessionExtensions.cs
+++ Snittlistan/Helpers/DocumentSessionExtensions.cs
@@ -9,19 +9,21 @@ namespace Snittlistan.Helpers
 	{
 		public static User FindUserByEmail(this IDocumentSession sess, string email)
 		{
-            return sess.Query<User>().FirstOrDefault(u => u.Email == email);
+            return sess.Query<User>()
+                .FirstOrDefault(u => u.Email == email);
 		}
 
-		public static IQueryable<User> FindUserByActivationKey(this IDocumentSession sess, string key)
+		public static User FindUserByActivationKey(this IDocumentSession sess, string key)
 		{
-			return sess.Query<User>().Where(u => u.ActivationKey == key);
+			return sess.Query<User>()
+                .FirstOrDefault(u => u.ActivationKey == key);
 		}
 
-		public static IQueryable<Match> FindByBitsId(this IDocumentSession sess, int id)
+		public static Match FindByBitsId(this IDocumentSession sess, int id)
 		{
 			return sess.Query<Match, Match_ByBitsMatchId>()
 				.Customize(c => c.WaitForNonStaleResultsAsOfNow())
-				.Where(m => m.BitsMatchId == id);
+				.FirstOrDefault(m => m.BitsMatchId == id);
 		}
 	}
 }
