e1313a1cad7f159a3e6f8b9c1a5cacb673821b83
Daniel Lidstrom <dlidstrom@gmail.com>
2011-11-09 22:47:29 +0100

    This is the correct way to test exposed services

--- Snittlistan.Test/AutoMapperInstallerTest.cs
+++ Snittlistan.Test/AutoMapperInstallerTest.cs
@@ -1,49 +1,46 @@
-﻿using System.Linq;
-using AutoMapper;
-using Castle.Core.Internal;
-using Castle.Windsor;
-using Snittlistan.Infrastructure;
-using Snittlistan.Installers;
-using Snittlistan.Models;
-using Snittlistan.ViewModels;
-using Xunit;
-
-namespace Snittlistan.Test
+﻿namespace Snittlistan.Test
 {
-	public class AutoMapperInstallerTest
-	{
-		private readonly IWindsorContainer container;
+    using System.Linq;
+    using AutoMapper;
+    using Castle.Core.Internal;
+    using Castle.Windsor;
+    using Snittlistan.Installers;
+    using Xunit;
 
-		public AutoMapperInstallerTest()
-		{
-			container = new WindsorContainer()
-				.Install(new AutoMapperInstaller());
-		}
+    public class AutoMapperInstallerTest
+    {
+        private readonly IWindsorContainer container;
 
-		[Fact]
-		public void AllProfilesImplementProfile()
-		{
-			var allHandlers = InstallerTestHelper.GetAllHandlers(container);
-			var profileHandlers = InstallerTestHelper.GetHandlersFor(typeof(Profile), container);
-			Assert.NotEmpty(allHandlers);
-			Assert.Equal(allHandlers, profileHandlers);
-		}
+        public AutoMapperInstallerTest()
+        {
+            container = new WindsorContainer()
+                .Install(new AutoMapperInstaller());
+        }
 
-		[Fact]
-		public void AllProfilesAreRegistered()
-		{
-			var allProfiles = InstallerTestHelper.GetPublicClassesFromApplicationAssembly(c => c.Is<Profile>());
-			var registeredProfiles = InstallerTestHelper.GetImplementationTypesFor(typeof(Profile), container);
-			Assert.Equal(allProfiles, registeredProfiles);
-		}
+        [Fact]
+        public void AllProfilesImplementProfile()
+        {
+            var allHandlers = InstallerTestHelper.GetAllHandlers(container);
+            var profileHandlers = InstallerTestHelper.GetHandlersFor(typeof(Profile), container);
+            Assert.NotEmpty(allHandlers);
+            Assert.Equal(allHandlers, profileHandlers);
+        }
 
-		[Fact]
-		public void AllProfilesExposeProfileAsService()
-		{
-			var profilesWithWrongService = InstallerTestHelper.GetHandlersFor(typeof(Profile), container)
-				.Where(c => c.ComponentModel.Services.Any(s => s != typeof(Profile)))
-				.ToArray();
-			Assert.Empty(profilesWithWrongService);
-		}
-	}
+        [Fact]
+        public void AllProfilesAreRegistered()
+        {
+            var allProfiles = InstallerTestHelper.GetPublicClassesFromApplicationAssembly(c => c.Is<Profile>());
+            var registeredProfiles = InstallerTestHelper.GetImplementationTypesFor(typeof(Profile), container);
+            Assert.Equal(allProfiles, registeredProfiles);
+        }
+
+        [Fact]
+        public void AllProfilesExposeProfileAsService()
+        {
+            var profilesWithWrongService = InstallerTestHelper.GetHandlersFor(typeof(Profile), container)
+                .Where(c => !c.ComponentModel.Services.SequenceEqual(new[] { typeof(Profile) }))
+                .ToArray();
+            Assert.Empty(profilesWithWrongService);
+        }
+    }
 }
