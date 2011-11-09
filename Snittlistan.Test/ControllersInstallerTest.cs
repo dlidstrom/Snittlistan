b07412e02e9b4d5a1da570544a3cca03587537ed
Daniel Lidstrom <dlidstrom@gmail.com>
2011-11-09 22:47:29 +0100

    This is the correct way to test exposed services

--- Snittlistan.Test/ControllersInstallerTest.cs
+++ Snittlistan.Test/ControllersInstallerTest.cs
@@ -1,75 +1,75 @@
-﻿using System.Linq;
-using System.Web.Mvc;
-using Castle.Core;
-using Castle.Core.Internal;
-using Castle.Windsor;
-using Snittlistan.Installers;
-using Xunit;
-
-namespace Snittlistan.Test
+﻿namespace Snittlistan.Test
 {
-	public class ControllersInstallerTest
-	{
-		private readonly IWindsorContainer container;
+    using System.Linq;
+    using System.Web.Mvc;
+    using Castle.Core;
+    using Castle.Core.Internal;
+    using Castle.Windsor;
+    using Snittlistan.Installers;
+    using Xunit;
 
-		public ControllersInstallerTest()
-		{
-			container = new WindsorContainer()
-				.Install(new ControllerInstaller());
-		}
+    public class ControllersInstallerTest
+    {
+        private readonly IWindsorContainer container;
 
-		[Fact]
-		public void AllControllersImplementIController()
-		{
-			var allHandlers = InstallerTestHelper.GetAllHandlers(container);
-			var controllerHandlers = InstallerTestHelper.GetHandlersFor(typeof(IController), container);
-			Assert.NotEmpty(allHandlers);
-			Assert.Equal(allHandlers, controllerHandlers);
-		}
+        public ControllersInstallerTest()
+        {
+            container = new WindsorContainer()
+                .Install(new ControllerInstaller());
+        }
 
-		[Fact]
-		public void AllControllersAreRegistered()
-		{
-			//// Is<TType> is a helper extension method from Windsor
-			//// which behaves like 'is' keyword in C# but at a Type, not instance level
+        [Fact]
+        public void AllControllersImplementIController()
+        {
+            var allHandlers = InstallerTestHelper.GetAllHandlers(container);
+            var controllerHandlers = InstallerTestHelper.GetHandlersFor(typeof(IController), container);
+            Assert.NotEmpty(allHandlers);
+            Assert.Equal(allHandlers, controllerHandlers);
+        }
 
-			var allControllers = InstallerTestHelper.GetPublicClassesFromApplicationAssembly(c => c.Is<IController>());
-			var registeredControllers = InstallerTestHelper.GetImplementationTypesFor(typeof(IController), container);
-			Assert.Equal(allControllers, registeredControllers);
-		}
+        [Fact]
+        public void AllControllersAreRegistered()
+        {
+            //// Is<TType> is a helper extension method from Windsor
+            //// which behaves like 'is' keyword in C# but at a Type, not instance level
 
-		[Fact]
-		public void AllAndOnlyControllersHaveControllerSuffix()
-		{
-			var allControllers = InstallerTestHelper.GetPublicClassesFromApplicationAssembly(c => c.Name.EndsWith("Controller"));
-			var registeredControllers = InstallerTestHelper.GetImplementationTypesFor(typeof(IController), container);
-			Assert.Equal(allControllers, registeredControllers);
-		}
+            var allControllers = InstallerTestHelper.GetPublicClassesFromApplicationAssembly(c => c.Is<IController>());
+            var registeredControllers = InstallerTestHelper.GetImplementationTypesFor(typeof(IController), container);
+            Assert.Equal(allControllers, registeredControllers);
+        }
 
-		[Fact]
-		public void AllAndOnlyControllersLiveInControllersNamespace()
-		{
-			var allControllers = InstallerTestHelper.GetPublicClassesFromApplicationAssembly(c => c.Namespace.Contains("Controllers"));
-			var registeredControllers = InstallerTestHelper.GetImplementationTypesFor(typeof(IController), container);
-			Assert.Equal(allControllers, registeredControllers);
-		}
+        [Fact]
+        public void AllAndOnlyControllersHaveControllerSuffix()
+        {
+            var allControllers = InstallerTestHelper.GetPublicClassesFromApplicationAssembly(c => c.Name.EndsWith("Controller"));
+            var registeredControllers = InstallerTestHelper.GetImplementationTypesFor(typeof(IController), container);
+            Assert.Equal(allControllers, registeredControllers);
+        }
 
-		[Fact]
-		public void AllControllersAreTransient()
-		{
-			var nonTransientControllers = InstallerTestHelper.GetHandlersFor(typeof(IController), container)
-				.Where(c => c.ComponentModel.LifestyleType != LifestyleType.Transient)
-				.ToArray();
-			Assert.Empty(nonTransientControllers);
-		}
+        [Fact]
+        public void AllAndOnlyControllersLiveInControllersNamespace()
+        {
+            var allControllers = InstallerTestHelper.GetPublicClassesFromApplicationAssembly(c => c.Namespace.Contains("Controllers"));
+            var registeredControllers = InstallerTestHelper.GetImplementationTypesFor(typeof(IController), container);
+            Assert.Equal(allControllers, registeredControllers);
+        }
 
-		[Fact]
-		public void AllControllersExposeThemselvesAsService()
-		{
-			var controllersWithWrongName = InstallerTestHelper.GetHandlersFor(typeof(IController), container)
-				.Where(c => c.ComponentModel.Services.Any(s => s != c.ComponentModel.Implementation))
-				.ToArray();
-			Assert.Empty(controllersWithWrongName);
-		}
-	}
+        [Fact]
+        public void AllControllersAreTransient()
+        {
+            var nonTransientControllers = InstallerTestHelper.GetHandlersFor(typeof(IController), container)
+                .Where(c => c.ComponentModel.LifestyleType != LifestyleType.Transient)
+                .ToArray();
+            Assert.Empty(nonTransientControllers);
+        }
+
+        [Fact]
+        public void AllControllersExposeThemselvesAsService()
+        {
+            var controllersWithWrongName = InstallerTestHelper.GetHandlersFor(typeof(IController), container)
+                .Where(c => !c.ComponentModel.Services.SequenceEqual(new[] { c.ComponentModel.Implementation }))
+                .ToArray();
+            Assert.Empty(controllersWithWrongName);
+        }
+    }
 }
