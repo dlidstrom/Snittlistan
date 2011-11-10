be387ca2eb62ec697d443c310ea94b7d9afbf72b
Daniel Lidstrom <dlidstrom@gmail.com>
2011-11-10 10:51:20 +0100

    Improved client side experience

---
+++ Snittlistan/Infrastructure/Indexes/Teams.cs
@@ -0,0 +1,31 @@
+﻿namespace Snittlistan.Infrastructure.Indexes
+{
+    using System.Linq;
+    using Raven.Client.Indexes;
+    using Snittlistan.Models;
+
+    public class Teams : AbstractIndexCreationTask<Match, Teams.Result>
+    {
+        public Teams()
+        {
+            Map = matches => from match in matches
+                             from team in match.Teams
+                             select new
+                             {
+                                 Team = team.Name,
+                             };
+
+            Reduce = results => from result in results
+                                group result by result.Team into g
+                                select new
+                                {
+                                    Team = g.Key
+                                };
+        }
+
+        public class Result
+        {
+            public string Team { get; set; }
+        }
+    }
+}
