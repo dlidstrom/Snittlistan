2370bce3bcf8b16757a10fe2dc41f24141362b87
Daniel Lidstrom <dlidstrom@gmail.com>
2011-11-11 12:09:20 +0100

    Added user by email index

---
+++ Snittlistan/Infrastructure/Indexes/User_ByEmail.cs
@@ -0,0 +1,15 @@
+﻿namespace Snittlistan.Infrastructure.Indexes
+{
+    using System.Linq;
+    using Raven.Client.Indexes;
+    using Snittlistan.Models;
+
+    public class User_ByEmail : AbstractIndexCreationTask<User>
+    {
+        public User_ByEmail()
+        {
+            Map = users => from user in users
+                           select new { user.Email };
+        }
+    }
+}
