68496ecba3ba6deb44558c0335e24eeeb7bff003
Daniel Lidstrom <dlidstrom@gmail.com>
2011-11-19 17:06:54 +0100

    Improved error handling
    Last-20 index may be out of date.

--- Snittlistan/ViewModels/PlayerStatsViewModel.cs
+++ Snittlistan/ViewModels/PlayerStatsViewModel.cs
@@ -1,7 +1,27 @@
 ﻿namespace Snittlistan.ViewModels
 {
+    using System.Collections.Generic;
+    using Snittlistan.Infrastructure.Indexes;
+
     public class PlayerStatsViewModel
     {
+        public PlayerStatsViewModel(Matches_PlayerStats.Result s, IDictionary<string, double> last20)
+        {
+            Player = s.Player;
+            Series = s.Series;
+            AverageScore = s.Score / s.Series;
+            AveragePins = s.Pins / s.Series;
+            Max = s.Max;
+            AverageStrikes = s.Strikes / s.Series;
+            AverageMisses = s.Misses / s.Series;
+            AverageOnePinMisses = s.OnePinMisses / s.Series;
+            AverageSplits = s.Splits / s.Series;
+            CoveredAll = s.CoveredAll;
+            double averageLast20 = 0.0;
+            last20.TryGetValue(s.Player, out averageLast20);
+            AverageLast20 = averageLast20;
+        }
+
         public string Player { get; set; }
         public int Series { get; set; }
         public double AverageScore { get; set; }
