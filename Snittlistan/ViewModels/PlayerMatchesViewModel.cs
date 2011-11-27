913eb69b102ab9024b451e3593f84cefaaa3d097
Daniel Lidstrom <dlidstrom@gmail.com>
2011-11-27 18:23:02 +0100

    Show additional player stats

--- Snittlistan/ViewModels/PlayerMatchesViewModel.cs
+++ Snittlistan/ViewModels/PlayerMatchesViewModel.cs
@@ -7,5 +7,7 @@
     {
         public string Player { get; set; }
         public List<Player_ByMatch.Result> Stats { get; set; }
+        public Matches_PlayerStats.Result Results { get; set; }
+        public Pins_Last20.Result Last20 { get; set; }
     }
 }
