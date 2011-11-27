19dbab71c0be89f9e7eb95e7839f3bea82c737af
Daniel Lidstrom <dlidstrom@gmail.com>
2011-11-27 18:42:52 +0100

    Unused code

--- Snittlistan/HtmlHelpers/CustomHelpers.cs
+++ Snittlistan/HtmlHelpers/CustomHelpers.cs
@@ -7,27 +7,6 @@
     public static class CustomHtmlHelpers
     {
         /// <summary>
-        /// Returns HTML with edit and back links. Only if request is authenticated,
-        /// is the edit link included.
-        /// </summary>
-        /// <param name="html">The HTML helper instance that this method extends.</param>
-        /// <param name="model">Match view model.</param>
-        /// <returns>The HTML markup with edit and back links.</returns>
-        public static MvcHtmlString GenerateEditBackLinks(this HtmlHelper html, MatchViewModel.MatchDetails model)
-        {
-            var builder = new TagBuilder("p");
-
-            if (html.ViewContext.HttpContext.Request.IsAuthenticated)
-            {
-                builder.InnerHtml = html.ActionLink("Redigera", "Edit", new { id = model.Id }).ToString() + " | ";
-            }
-
-            builder.InnerHtml += html.ActionLink("Tillbaka", "Index");
-
-            return new MvcHtmlString(builder.ToString());
-        }
-
-        /// <summary>
         /// Returns HTML with an anchor link to BITS for the match.
         /// </summary>
         /// <param name="html">The HTML helper instance that this method extends.</param>
