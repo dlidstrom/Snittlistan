fea99c7da5a17a95b62d1ace06b6259a05ea8b0d
Daniel Lidstrom <dlidstrom@gmail.com>
2011-11-27 18:42:40 +0100

    Display internal links as buttons

---
+++ Snittlistan/Content/js/button.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="jquery-1.6.4-vsdoc.js" />
+/// <reference path="jquery-ui.js" />
+
+$(function () {
+    $("a[data-button]").each(function () {
+        $(this).button();
+    });
+});
