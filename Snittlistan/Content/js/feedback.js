9a18e625ab1c90f0a263b2851fe422f6ca9a6118
Daniel Lidstrom <dlidstrom@gmail.com>
2011-11-27 18:16:45 +0100

    Moved feedback code to own file

---
+++ Snittlistan/Content/js/feedback.js
@@ -0,0 +1,7 @@
+﻿/// <reference path="jquery-1.6.4-vsdoc.js" />
+
+$(function () {
+    $("#feedbackTab").click(function () {
+        window.location.href = "mailto:dlidstrom@gmail.com?Subject=Önskemål Snittlistan";
+    });
+});
