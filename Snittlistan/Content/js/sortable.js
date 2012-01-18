a83eefde4add007f78d865ef812800dcd83b8de5
Daniel Lidstrom <dlidstrom@gmail.com>
2012-01-18 21:46:17 +0100

    Players table is now sortable

---
+++ Snittlistan/Content/js/sortable.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="jquery-1.6.4-vsdoc.js" />
+
+$(function () {
+    var options = { cancelSelection: true };
+    $('table[data-sortable="true"]').each(function () {
+        $(this).tablesorter(options);
+    });
+});
