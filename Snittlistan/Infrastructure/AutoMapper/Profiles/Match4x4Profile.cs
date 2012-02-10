8ca28ac2a7c35fda7bfeeb517f5d47594d311071
Daniel Lidstrom <dlidstrom@gmail.com>
2012-02-10 09:49:22 +0100

    No need to shorten name here

--- Snittlistan/Infrastructure/AutoMapper/Profiles/Match4x4Profile.cs
+++ Snittlistan/Infrastructure/AutoMapper/Profiles/Match4x4Profile.cs
@@ -11,8 +11,7 @@
             global::AutoMapper.Mapper.CreateMap<Match4x4, Match4x4ViewModel.MatchDetails>();
             global::AutoMapper.Mapper.CreateMap<Team4x4, Team4x4DetailsViewModel>();
             global::AutoMapper.Mapper.CreateMap<Serie4x4, Team4x4DetailsViewModel.Serie>();
-            global::AutoMapper.Mapper.CreateMap<Game4x4, Team4x4DetailsViewModel.Game>()
-                .ForMember(vm => vm.Player, o => o.MapFrom(g => NameShortener.Shorten(g.Player)));
+            global::AutoMapper.Mapper.CreateMap<Game4x4, Team4x4DetailsViewModel.Game>();
             global::AutoMapper.Mapper.CreateMap<Team4x4, Team4x4ViewModel>()
                 .ForMember(vm => vm.Serie1, o => o.ResolveUsing(new SerieResolver { Serie = 0 }))
                 .ForMember(vm => vm.Serie2, o => o.ResolveUsing(new SerieResolver { Serie = 1 }))
