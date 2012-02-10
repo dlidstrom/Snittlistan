ff0e2ebce2ffd70b181edd04c6cf3e0b7af2e982
Daniel Lidstrom <dlidstrom@gmail.com>
2012-02-10 09:49:09 +0100

    Index both types of matches

--- Snittlistan/Infrastructure/Indexes/Teams.cs
+++ Snittlistan/Infrastructure/Indexes/Teams.cs
@@ -4,13 +4,17 @@
     using Raven.Client.Indexes;
     using Snittlistan.Models;
 
-    public class Teams : AbstractIndexCreationTask<Match8x4, Teams.Result>
+    public class Teams : AbstractMultiMapIndexCreationTask<Teams.Result>
     {
         public Teams()
         {
-            Map = matches => from match in matches
-                             from team in match.Teams
-                             select new { Team = team.Name };
+            AddMap<Match8x4>(matches => from match in matches
+                                        from team in match.Teams
+                                        select new { Team = team.Name });
+
+            AddMap<Match4x4>(matches => from match in matches
+                                        from team in match.Teams
+                                        select new { Team = team.Name });
 
             Reduce = results => from result in results
                                 group result by result.Team into g
