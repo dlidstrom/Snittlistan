862b8109c3bb5c879f79bfe2bc1fb7c506e58442
Daniel Lidstrom <dlidstrom@gmail.com>
2012-07-11 10:07:36 +0200

    A few javascript objects and their specifications
    None of them really used yet. Will change a lot.

---
+++ Snittlistan/Content/js/specs/specs.turn-view.js
@@ -0,0 +1,17 @@
+﻿describe("Turn view", function () {
+    var server = sinon.fakeServer.create();
+    var list = [{ }, { }, { }];
+
+    beforeEach(function () {
+        server.respondWith(
+            'GET',
+            /\/appointments/ ,
+            [200,
+                { "Content-Type": "application/json" }, JSON.stringify(list)]
+        );
+    });
+
+    it("lists all matches", function () {
+        expect($(".row-fluid", ".span4").length).toEqual(3);
+    });
+});
---
+++ Snittlistan/Content/js/specs/specs.turn.js
@@ -0,0 +1,34 @@
+﻿describe("Turn", function () {
+    "use strict";
+    var turn;
+
+    beforeEach(function () {
+        turn = new App.Turn({
+            season: '2011-2012',
+            number: 1,
+            date_start: new Date('September 9, 2012'),
+            date_end: new Date('September 10, 2012')
+        });
+    });
+
+    it("should have a season", function () {
+        expect(turn.get("season")).toEqual('2011-2012');
+    });
+
+    it("should have a number", function () {
+        expect(turn.get("number")).toEqual(1);
+    });
+
+    it("should have a start date", function () {
+        expect(turn.get("date_start")).toEqual(new Date('September 9, 2012'));
+    });
+
+    it("should have an end date", function () {
+        expect(turn.get("date_end")).toEqual(new Date('September 10, 2012'));
+    });
+
+    it("can change number", function () {
+        turn.set({ number: 2 });
+        expect(turn.get('number')).toEqual(2);
+    });
+});
