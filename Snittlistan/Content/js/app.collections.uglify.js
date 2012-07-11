1566b31fb3f16b7524f96e055b7c4db63df7eb8b
Daniel Lidstrom <dlidstrom@gmail.com>
2012-07-11 16:14:49 +0200

    Split javascript into separate files
    Models, collections, views, and utilities are split into their own file.

---
+++ Snittlistan/Content/js/app.collections.uglify.js
@@ -0,0 +1,12 @@
+﻿$(function ($, backbone, _, handlebars, app, undefined) {
+    "use strict";
+    var collections = { };
+
+    // turns collection
+    collections.Turns = backbone.Collection.extend({
+        model: app.Models.Turn,
+        url: '/turns'
+    });
+
+    app.Collections = collections;
+}($, Backbone, _, Handlebars, window.App || { }));
---
+++ Snittlistan/Content/js/app.utils.uglify.js
@@ -0,0 +1,21 @@
+﻿(function ($, handlebars, app, undefined) {
+    "use strict";
+
+    app.TemplateCache = {
+        get: function (selector) {
+            this.templates = this.templates || { };
+
+            var template = this.templates[selector];
+            if (!template) {
+                var tmpl = $(selector).html();
+                if (typeof tmpl === undefined || tmpl === null) {
+                    throw new Error("TemplateCache: " + selector + " not found");
+                }
+                template = handlebars.compile(tmpl);
+                this.templates[selector] = template;
+            }
+
+            return template;
+        }
+    };
+}($, Handlebars, window.App = window.App || { }));
