7a56252a5e309a6b1079a99e57b01684a54270ce
Daniel Lidstrom <dlidstrom@gmail.com>
2012-08-06 13:54:26 +0200

    Collections are really a part of the models

--- Snittlistan/Content/js/app/app.models.js
+++ Snittlistan/Content/js/app/app.models.js
@@ -11,4 +11,14 @@
     });
 
     app.Models = models;
+
+    var collections = { };
+
+    // turns collection
+    collections.Turns = backbone.Collection.extend({
+        model: app.Models.Turn,
+        url: '/turns'
+    });
+
+    app.Collections = collections;
 }($, Backbone, window.App = window.App || { }));
