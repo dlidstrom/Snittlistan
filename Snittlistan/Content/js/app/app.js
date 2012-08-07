b9a2ddfcec310d796156d60d3263c82eadca653d
Daniel Lidstrom <dlidstrom@gmail.com>
2012-08-07 11:48:16 +0200

    Can now render the header
    Initial data is stored in App.initial_data. This will hopefully speed up
    the first rendering, when there's data to fill the view directly.

---
+++ Snittlistan/Content/js/app/app.js
@@ -0,0 +1,31 @@
+﻿// @reference app.models.js
+// @reference app.views.js
+(function ($, backbone, app, undefined) {
+    "use strict";
+
+    //var turns = new Turns();
+    //turns.fetch();
+    var turns = new app.Collections.Turns();
+    turns.reset(app.initial_data);
+    app.turns = turns;
+
+    // render
+    var turns_view = new app.Views.Turns({ collection: turns });
+    turns_view.render();
+
+    // application router
+    var router = backbone.Router.extend({
+        initialize: function () {
+            $("#header").html(new app.Views.Header().render());
+        },
+        routes: {
+            "": "home"
+        },
+        home: function () {
+        }
+    });
+
+    app.Router = router;
+    app.router = new app.Router();
+    backbone.history.start();
+}($, Backbone, window.App = window.App || { }));
