14cdb93eafac923922ab3277a5fa1007ddc4dae7
Daniel Lidstrom <dlidstrom@gmail.com>
2012-08-13 13:13:29 +0200

    Try to get tests running
    Need to integrate with mvc in order to make this work. Templates need to
    be compiled, so cassette must be used.

--- Snittlistan/Content/js/specs/specs.turn-view.js
+++ Snittlistan/Content/js/specs/specs.turn-view.js
@@ -1,17 +1,48 @@
 ﻿describe("Turn view", function () {
-    var server = sinon.fakeServer.create();
-    var list = [{ }, { }, { }];
+    var turnsView;
 
     beforeEach(function () {
-        server.respondWith(
-            'GET',
-            /\/appointments/ ,
-            [200,
-                { "Content-Type": "application/json" }, JSON.stringify(list)]
-        );
+        var turns = new App.Collections.Turns();
+        turns.reset([
+            {
+                turn: 2,
+                startDate: '22 sep',
+                endDate: '23 sep',
+                rosters: [
+                    {
+                        team: 'Fredrikshof A',
+                        location: 'Birka',
+                        opponent: 'Stockholm IFK',
+                        date: 'lördag den 22 september 2012, 10:00',
+                        declined: {
+                            count: 2,
+                            names: 'Kjell Jansson, Christer Liedholm'
+                        }
+                    },
+                    {
+                        team: 'Fredrikshof F',
+                        location: 'Bowl-O-Rama',
+                        opponent: 'AIK F',
+                        date: 'lördag den 22 september 2012, 11:40',
+                        declined: {
+                            count: 1,
+                            names: 'Daniel Lidström'
+                        }
+                    },
+                    {
+                        team: 'Fredrikshof B',
+                        location: 'Bowl-O-Rama',
+                        opponent: 'Hellas B',
+                        date: 'söndag den 23 september 2012, 10:00',
+                        declined: null
+                    }
+                ]
+            }
+        ]);
+        this.turnsView = new App.Views.Turns({ model: turns });
     });
 
     it("lists all matches", function () {
-        expect($(".row-fluid", ".span4").length).toEqual(3);
+        expect(this.turnsView.$(".row-fluid", ".span4").length).toEqual(3);
     });
 });
