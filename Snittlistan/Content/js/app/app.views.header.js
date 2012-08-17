774113b81d96097f37134879b9bd3efbfa734eef
Daniel Lidstrom <dlidstrom@gmail.com>
2012-08-17 14:15:55 +0200

    Better routes handling and app state notification
    When routes change the app state is notified automatically
    by the router.

--- Snittlistan/Content/js/app/app.views.header.js
+++ Snittlistan/Content/js/app/app.views.header.js
@@ -11,9 +11,14 @@
         tagName: 'section',
         className: 'navbar navbar-fixed-top',
         template: window.JST['header-template'],
-        initialize: function () {
+        events: {
+            'click li#menu-players a': 'players',
+            'click li#menu-turns a': 'turns'
+        },
+        initialize: function (options) {
             _.bindAll(this);
             this.model.on('change', this.render);
+            this.router = options.router;
         },
         beforeClose: function () {
             this.model.off('change', this.render);
@@ -21,6 +26,14 @@
         render: function () {
             this.$el.html(this.template.render(this.model.toJSON()));
             return this;
+        },
+        players: function () {
+            this.router.navigate('players', { trigger: true });
+            return false;
+        },
+        turns: function () {
+            this.router.navigate('turns', { trigger: true });
+            return false;
         }
     });
 
