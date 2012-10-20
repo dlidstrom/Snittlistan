3cfd289a92f5099df12279474aa1f82080cab6a6
Daniel Lidstrom <dlidstrom@gmail.com>
2012-10-20 21:37:52 +0200

    Typeahead when registering new rosters

---
+++ Snittlistan.Web/Infrastructure/Indexes/RosterSearchTerms.cs
@@ -0,0 +1,33 @@
+﻿namespace Snittlistan.Web.Infrastructure.Indexes
+{
+    using System.Linq;
+
+    using Raven.Abstractions.Indexing;
+    using Raven.Client.Indexes;
+
+    using Snittlistan.Web.Models;
+
+    public class RosterSearchTerms : AbstractIndexCreationTask<Roster, RosterSearchTerms.Result>
+    {
+        public RosterSearchTerms()
+        {
+            this.Map = rosters => from roster in rosters
+                                  select new
+                                  {
+                                      roster.Team,
+                                      roster.Opponent,
+                                      roster.Location,
+                                  };
+            this.Store(x => x.Team, FieldStorage.Yes);
+        }
+
+        public class Result
+        {
+            public string Team { get; set; }
+
+            public string Opponent { get; set; }
+
+            public string Location { get; set; }
+        }
+    }
+}
