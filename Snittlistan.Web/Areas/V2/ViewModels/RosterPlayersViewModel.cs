a0987d00446ac2802df7cdf2f0e05b1d81444c66
Daniel Lidstrom <dlidstrom@gmail.com>
2012-10-27 17:27:31 +0200

    Added validation

--- Snittlistan.Web/Areas/V2/ViewModels/RosterPlayersViewModel.cs
+++ Snittlistan.Web/Areas/V2/ViewModels/RosterPlayersViewModel.cs
@@ -1,15 +1,33 @@
 namespace Snittlistan.Web.Areas.V2.ViewModels
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class RosterPlayersViewModel
     {
+        [Required(ErrorMessage = "Ange namn på spelare 1")]
         public string Table1Player1 { get; set; }
+
+        [Required(ErrorMessage = "Ange namn på spelare 2")]
         public string Table1Player2 { get; set; }
+
+        [Required(ErrorMessage = "Ange namn på spelare 3")]
         public string Table2Player1 { get; set; }
+
+        [Required(ErrorMessage = "Ange namn på spelare 4")]
         public string Table2Player2 { get; set; }
+
+        [Required(ErrorMessage = "Ange namn på spelare 5")]
         public string Table3Player1 { get; set; }
+
+        [Required(ErrorMessage = "Ange namn på spelare 6")]
         public string Table3Player2 { get; set; }
+
+        [Required(ErrorMessage = "Ange namn på spelare 7")]
         public string Table4Player1 { get; set; }
+
+        [Required(ErrorMessage = "Ange namn på spelare 8")]
         public string Table4Player2 { get; set; }
+
         public string Reserve { get; set; }
     }
 }
