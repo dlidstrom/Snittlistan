adc01bb7e1924474533869efc509a9f77a021f88
Daniel Lidstrom <dlidstrom@gmail.com>
2013-07-03 19:41:39 +0200

    Closes #218

---
+++ Snittlistan.Test/TeamViewModelTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Snittlistan.Web.Areas.V2.Domain;
+using Snittlistan.Web.Areas.V2.ReadModels;
+using Snittlistan.Web.Areas.V2.ViewModels;
+using Xunit;
+
+namespace Snittlistan.Test
+{
+    public class TeamViewModelTest
+    {
+        [Fact]
+        public void GroupsByTurn()
+        {
+            // Arrange
+            var player1 = new Player("Daniel", "e@d.com", false) { Id = "9876" };
+            var player2 = new Player("Tomas", "s@d.com", false) { Id = "8765" };
+            var teamOfWeek1 = new TeamOfWeek(1234, 2012, 10, "Team A");
+            teamOfWeek1.AddResultForPlayer(player1, 210);
+            teamOfWeek1.AddResultForPlayer(player2, 190);
+            var teamOfWeek2 = new TeamOfWeek(5432, 2012, 10, "Team B");
+            teamOfWeek2.AddResultForPlayer(player1, 220);
+            teamOfWeek2.AddResultForPlayer(player2, 180);
+
+            // Act
+            var vm = new TeamOfWeekViewModel(2012, new List<TeamOfWeek>
+            {
+                teamOfWeek1,
+                teamOfWeek2
+            });
+
+            // Assert
+            var weeks = vm.Weeks;
+            Assert.Equal(1, weeks.Count);
+            var week = weeks[0];
+            Assert.Equal(10, week.Turn);
+            var players = week.Players;
+            Assert.Equal(2, players.Count);
+            var playerScore1 = players[0];
+            Assert.Equal("Daniel", playerScore1.Name);
+            Assert.Equal(220, playerScore1.Pins);
+            Assert.Equal(1, playerScore1.Series);
+            var playerScore2 = players[1];
+            Assert.Equal("Tomas", playerScore2.Name);
+            Assert.Equal(190, playerScore2.Pins);
+            Assert.Equal(1, playerScore2.Series);
+            Assert.Equal(410, week.Top8);
+        }
+    }
+}
--- Snittlistan.Web/Areas/V2/ReadModels/TeamOfWeek.cs
+++ Snittlistan.Web/Areas/V2/ReadModels/TeamOfWeek.cs
@@ -6,12 +6,13 @@ namespace Snittlistan.Web.Areas.V2.ReadModels
 {
     public class TeamOfWeek : IReadModel
     {
-        public TeamOfWeek(int bitsMatchId, int season, int turn)
+        public TeamOfWeek(int bitsMatchId, int season, int turn, string team)
         {
             PlayerScores = new Dictionary<string, PlayerScore>();
             Id = IdFromBitsMatchId(bitsMatchId);
             Season = season;
             Turn = turn;
+            Team = team;
         }
 
         public string Id { get; private set; }
@@ -19,6 +20,8 @@ namespace Snittlistan.Web.Areas.V2.ReadModels
         public Dictionary<string, PlayerScore> PlayerScores { get; private set; }
 
         public int Season { get; private set; }
+
+        public string Team { get; private set; }
 
         public int Turn { get; private set; }
 
@@ -31,22 +34,29 @@ namespace Snittlistan.Web.Areas.V2.ReadModels
         {
             if (PlayerScores.ContainsKey(player.Id) == false)
             {
-                PlayerScores.Add(player.Id, new PlayerScore(player.Name));
+                PlayerScores.Add(player.Id, new PlayerScore(player.Name, Team));
             }
 
-            PlayerScores[player.Id].Pins += pins;
+            var playerScore = PlayerScores[player.Id];
+            playerScore.Pins += pins;
+            playerScore.Series++;
         }
 
         public class PlayerScore
         {
-            public PlayerScore(string name)
+            public PlayerScore(string name, string team)
             {
+                Team = team;
                 Name = name;
             }
 
             public string Name { get; private set; }
 
             public int Pins { get; set; }
+
+            public int Series { get; set; }
+
+            public string Team { get; private set; }
         }
     }
 }
