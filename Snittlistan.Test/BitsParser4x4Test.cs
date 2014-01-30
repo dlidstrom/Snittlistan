a7dfb89789394112d0b07f01e8b88a4abcf5f5cb
Daniel Lidstrom <dlidstrom@gmail.com>
2014-01-30 21:53:09 +0100

    Fixed BITS match parsing for 4x4 matches

--- Snittlistan.Test/BitsParser4x4Test.cs
+++ Snittlistan.Test/BitsParser4x4Test.cs
@@ -15,105 +15,105 @@ namespace Snittlistan.Test
         {
             homePlayers = new[]
             {
-                new Player("Jennie Bäck", "e@d.com", Player.Status.Active) { Id = "player-1" },
-                new Player("Gunvor Magnusson", "e@d.com", Player.Status.Active) { Id = "player-2" },
-                new Player("Elsy Lundin", "e@d.com", Player.Status.Active) { Id = "player-3" },
-                new Player("Kristina Wikström", "e@d.com", Player.Status.Active) { Id = "player-4" },
-                new Player("Anita Enström", "e@d.com", Player.Status.Active) { Id = "player-5" }
+                new Player("Markus Norbeck", "e@d.com", Player.Status.Active) { Id = "player-1" },
+                new Player("Lars Norbeck", "e@d.com", Player.Status.Active) { Id = "player-2" },
+                new Player("Daniel Solvander", "e@d.com", Player.Status.Active) { Id = "player-3" },
+                new Player("Matz Classon", "e@d.com", Player.Status.Active) { Id = "player-4" }
             };
             awayPlayers = new[]
             {
-                new Player("Stefan Markenfelt", "e@d.com", Player.Status.Active) { Id = "player-1" },
-                new Player("Markus Norbeck", "e@d.com", Player.Status.Active) { Id = "player-2" },
-                new Player("Daniel Solvander", "e@d.com", Player.Status.Active) { Id = "player-3" },
-                new Player("Per-Erik Freij", "e@d.com", Player.Status.Active) { Id = "player-4" }
+                new Player("Tobias Wiklund", "e@d.com", Player.Status.Active) { Id = "player-5" },
+                new Player("Wiktor Svensson", "e@d.com", Player.Status.Active) { Id = "player-6" },
+                new Player("Moa Nilsson", "e@d.com", Player.Status.Active) { Id = "player-7" },
+                new Player("Rebecka Wahlström", "e@d.com", Player.Status.Active) { Id = "player-8" },
+                new Player("Hanna Engevall", "e@d.com", Player.Status.Active) { Id = "player-9" }
             };
-        }
-
-        [Fact]
-        public void CanParseAwayTeam()
-        {
-            // Arrange
-            const string Team = "Fredrikshof B";
-
-            // Act
-            var result = new BitsParser(awayPlayers).Parse4(Resources.Id3060803, Team);
-            Assert.Equal(8, result.TeamScore);
-            Assert.Equal(12, result.OpponentScore);
-            var series = result.Series;
-
-            // Assert
-            Assert.Equal(4, series.Length);
-            var serie1 = series[0];
-            Assert.Equal(4, serie1.Games.Count);
-            VerifyGame(serie1.Games[0], Tuple.Create(0, "player-1", 157));
-            VerifyGame(serie1.Games[1], Tuple.Create(0, "player-2", 140));
-            VerifyGame(serie1.Games[2], Tuple.Create(1, "player-3", 167));
-            VerifyGame(serie1.Games[3], Tuple.Create(0, "player-4", 137));
-
-            var serie2 = series[1];
-            Assert.Equal(4, serie2.Games.Count);
-            VerifyGame(serie2.Games[0], Tuple.Create(0, "player-4", 134));
-            VerifyGame(serie2.Games[1], Tuple.Create(0, "player-3", 124));
-            VerifyGame(serie2.Games[2], Tuple.Create(0, "player-2", 132));
-            VerifyGame(serie2.Games[3], Tuple.Create(1, "player-1", 160));
-
-            var serie3 = series[2];
-            Assert.Equal(4, serie3.Games.Count);
-            VerifyGame(serie3.Games[0], Tuple.Create(1, "player-2", 161));
-            VerifyGame(serie3.Games[1], Tuple.Create(1, "player-1", 172));
-            VerifyGame(serie3.Games[2], Tuple.Create(0, "player-4", 145));
-            VerifyGame(serie3.Games[3], Tuple.Create(0, "player-3", 110));
-
-            var serie4 = series[3];
-            Assert.Equal(4, serie4.Games.Count);
-            VerifyGame(serie4.Games[0], Tuple.Create(1, "player-3", 148));
-            VerifyGame(serie4.Games[1], Tuple.Create(0, "player-4", 158));
-            VerifyGame(serie4.Games[2], Tuple.Create(1, "player-1", 164));
-            VerifyGame(serie4.Games[3], Tuple.Create(0, "player-2", 144));
         }
 
         [Fact]
         public void CanParseHomeTeam()
         {
             // Arrange
-            const string Team = "BK Ringen";
+            const string Team = "Fredrikshof B";
 
             // Act
-            var result = new BitsParser(homePlayers).Parse4(Resources.Id3060803, Team);
-            Assert.Equal(12, result.TeamScore);
-            Assert.Equal(8, result.OpponentScore);
+            var result = new BitsParser(homePlayers).Parse4(Resources.Id3060835, Team);
+            Assert.Equal(18, result.TeamScore);
+            Assert.Equal(1, result.OpponentScore);
             var series = result.Series;
 
             // Assert
             Assert.Equal(4, series.Length);
             var serie1 = series[0];
             Assert.Equal(4, serie1.Games.Count);
-            VerifyGame(serie1.Games[0], Tuple.Create(1, "player-1", 201));
-            VerifyGame(serie1.Games[1], Tuple.Create(1, "player-2", 166));
-            VerifyGame(serie1.Games[2], Tuple.Create(0, "player-3", 160));
-            VerifyGame(serie1.Games[3], Tuple.Create(1, "player-4", 169));
+            VerifyGame(serie1.Games[0], Tuple.Create(0, "player-1", 138));
+            VerifyGame(serie1.Games[1], Tuple.Create(1, "player-2", 178));
+            VerifyGame(serie1.Games[2], Tuple.Create(1, "player-3", 183));
+            VerifyGame(serie1.Games[3], Tuple.Create(1, "player-4", 131));
 
             var serie2 = series[1];
             Assert.Equal(4, serie2.Games.Count);
-            VerifyGame(serie2.Games[0], Tuple.Create(1, "player-5", 167));
-            VerifyGame(serie2.Games[1], Tuple.Create(1, "player-4", 192));
-            VerifyGame(serie2.Games[2], Tuple.Create(1, "player-1", 145));
-            VerifyGame(serie2.Games[3], Tuple.Create(0, "player-2", 147));
+            VerifyGame(serie2.Games[0], Tuple.Create(1, "player-3", 152));
+            VerifyGame(serie2.Games[1], Tuple.Create(1, "player-4", 189));
+            VerifyGame(serie2.Games[2], Tuple.Create(1, "player-1", 205));
+            VerifyGame(serie2.Games[3], Tuple.Create(1, "player-2", 136));
 
             var serie3 = series[2];
             Assert.Equal(4, serie3.Games.Count);
-            VerifyGame(serie3.Games[0], Tuple.Create(0, "player-4", 138));
-            VerifyGame(serie3.Games[1], Tuple.Create(0, "player-5", 162));
-            VerifyGame(serie3.Games[2], Tuple.Create(1, "player-2", 147));
-            VerifyGame(serie3.Games[3], Tuple.Create(1, "player-3", 128));
+            VerifyGame(serie3.Games[0], Tuple.Create(1, "player-4", 223));
+            VerifyGame(serie3.Games[1], Tuple.Create(1, "player-3", 251));
+            VerifyGame(serie3.Games[2], Tuple.Create(1, "player-2", 158));
+            VerifyGame(serie3.Games[3], Tuple.Create(1, "player-1", 149));
 
             var serie4 = series[3];
             Assert.Equal(4, serie4.Games.Count);
-            VerifyGame(serie4.Games[0], Tuple.Create(0, "player-2", 143));
-            VerifyGame(serie4.Games[1], Tuple.Create(1, "player-1", 184));
-            VerifyGame(serie4.Games[2], Tuple.Create(0, "player-4", 129));
-            VerifyGame(serie4.Games[3], Tuple.Create(1, "player-5", 151));
+            VerifyGame(serie4.Games[0], Tuple.Create(1, "player-2", 179));
+            VerifyGame(serie4.Games[1], Tuple.Create(1, "player-1", 181));
+            VerifyGame(serie4.Games[2], Tuple.Create(1, "player-4", 183));
+            VerifyGame(serie4.Games[3], Tuple.Create(0, "player-3", 167));
+        }
+
+        [Fact]
+        public void CanParseAwayTeam()
+        {
+            // Arrange
+            const string Team = "Trippel XXX";
+
+            // Act
+            var result = new BitsParser(awayPlayers).Parse4(Resources.Id3060835, Team);
+            Assert.Equal(1, result.TeamScore);
+            Assert.Equal(18, result.OpponentScore);
+            var series = result.Series;
+
+            // Assert
+            Assert.Equal(4, series.Length);
+            var serie1 = series[0];
+            Assert.Equal(4, serie1.Games.Count);
+            VerifyGame(serie1.Games[0], Tuple.Create(1, "player-5", 160));
+            VerifyGame(serie1.Games[1], Tuple.Create(0, "player-6", 117));
+            VerifyGame(serie1.Games[2], Tuple.Create(0, "player-7", 139));
+            VerifyGame(serie1.Games[3], Tuple.Create(0, "player-8", 83));
+
+            var serie2 = series[1];
+            Assert.Equal(4, serie2.Games.Count);
+            VerifyGame(serie2.Games[0], Tuple.Create(0, "player-8", 122));
+            VerifyGame(serie2.Games[1], Tuple.Create(0, "player-7", 156));
+            VerifyGame(serie2.Games[2], Tuple.Create(0, "player-6", 101));
+            VerifyGame(serie2.Games[3], Tuple.Create(0, "player-5", 133));
+
+            var serie3 = series[2];
+            Assert.Equal(4, serie3.Games.Count);
+            VerifyGame(serie3.Games[0], Tuple.Create(0, "player-6", 148));
+            VerifyGame(serie3.Games[1], Tuple.Create(0, "player-5", 142));
+            VerifyGame(serie3.Games[2], Tuple.Create(0, "player-9", 118));
+            VerifyGame(serie3.Games[3], Tuple.Create(0, "player-7", 136));
+
+            var serie4 = series[3];
+            Assert.Equal(4, serie4.Games.Count);
+            VerifyGame(serie4.Games[0], Tuple.Create(0, "player-7", 134));
+            VerifyGame(serie4.Games[1], Tuple.Create(0, "player-9", 119));
+            VerifyGame(serie4.Games[2], Tuple.Create(0, "player-5", 122));
+            VerifyGame(serie4.Games[3], Tuple.Create(0, "player-6", 167));
         }
 
         private static void VerifyGame(ResultSeries4ReadModel.Game game, Tuple<int, string, int> expected)
