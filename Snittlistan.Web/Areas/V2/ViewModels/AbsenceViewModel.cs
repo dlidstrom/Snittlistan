d64a1c1e568885901334949accdbe6f0b6dabfeb
Daniel Lidstrom <dlidstrom@gmail.com>
2014-04-04 12:13:04 +0200

    Cosmetic improvements mostly

--- Snittlistan.Web/Areas/V2/ViewModels/AbsenceViewModel.cs
+++ Snittlistan.Web/Areas/V2/ViewModels/AbsenceViewModel.cs
@@ -1,11 +1,21 @@
-﻿namespace Snittlistan.Web.Areas.V2.ViewModels
+﻿using System;
+using Snittlistan.Web.Areas.V2.Domain;
+
+namespace Snittlistan.Web.Areas.V2.ViewModels
 {
     public class AbsenceViewModel
     {
-        public string Player { get; set; }
+        public AbsenceViewModel(Absence absence, Player player)
+        {
+            PlayerName = player.Name;
+            From = absence.From;
+            To = absence.To;
+        }
 
-        public string From { get; set; }
+        public string PlayerName { get; private set; }
 
-        public string To { get; set; }
+        public DateTime From { get; private set; }
+
+        public DateTime To { get; private set; }
     }
 }
