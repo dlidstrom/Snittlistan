e57f9e13c1926b4b09e2a0fa05fbc0ece232dcc8
Daniel Lidstrom <dlidstrom@gmail.com>
2014-08-09 20:10:18 +0200

    Fixes problem with registration

---
+++ Snittlistan.Web/Areas/V2/Domain/ParseHeaderResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Snittlistan.Web.Areas.V2.Domain
+{
+    public class ParseHeaderResult
+    {
+        public ParseHeaderResult(string team, string opponent, DateTime date, string location)
+        {
+            if (team == null) throw new ArgumentNullException("team");
+            if (opponent == null) throw new ArgumentNullException("opponent");
+            if (location == null) throw new ArgumentNullException("location");
+            Team = team;
+            Opponent = opponent;
+            Date = date;
+            Location = location;
+        }
+
+        public string Team { get; private set; }
+
+        public string Opponent { get; private set; }
+
+        public DateTime Date { get; private set; }
+
+        public string Location { get; private set; }
+    }
+}
---
+++ Snittlistan.Web/Areas/V2/Domain/ParseResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Snittlistan.Web.Areas.V2.ReadModels;
+
+namespace Snittlistan.Web.Areas.V2.Domain
+{
+    public class ParseResult
+    {
+        public ParseResult(int teamScore, int awayScore, ResultSeriesReadModel.Serie[] series)
+        {
+            if (series == null) throw new ArgumentNullException("series");
+            Series = series;
+            OpponentScore = awayScore;
+            TeamScore = teamScore;
+        }
+
+        public int TeamScore { get; private set; }
+
+        public int OpponentScore { get; private set; }
+
+        public ResultSeriesReadModel.Serie[] Series { get; private set; }
+    }
+}
