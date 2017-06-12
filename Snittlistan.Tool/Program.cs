53c9325ca6e85f03248e7510ac34eac50e83f554
Daniel Lidstrom <dlidstrom@gmail.com>
2017-06-12 12:12:04 +0200

    Tool for exporting database

---
+++ Snittlistan.Tool/Program.cs
@@ -0,0 +1,25 @@
+﻿using Raven.Abstractions.Smuggler;
+using Raven.Client.Embedded;
+using Raven.Database.Smuggler;
+
+namespace Snittlistan.Tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var documentStore = new EmbeddableDocumentStore
+            {
+                DataDirectory = args[0]
+            })
+            {
+                documentStore.Initialize();
+                var dumper = new DataDumper(documentStore.DocumentDatabase, new SmugglerOptions());
+                dumper.ExportData(new SmugglerOptions
+                {
+                    BackupPath = "raven.dump"
+                });
+            }
+        }
+    }
+}
