b79ebc1255ddddee8a138df4584c73976b8993a7
Daniel Lidstrom <dlidstrom@gmail.com>
2018-01-21 22:46:59 +0100

    Added service installer

---
+++ Snittlistan.Queue.WindowsServiceHost/ProjectInstaller.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace Snittlistan.Queue.WindowsServiceHost
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+        }
+    }
+}
