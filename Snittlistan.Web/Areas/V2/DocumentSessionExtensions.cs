954fe6eaaaa9851246569db7a7e818b2fc3cf597
Daniel Lidstrom <dlidstrom@gmail.com>
2018-01-25 22:32:51 +0100

    Correct filtering

--- Snittlistan.Web/Areas/V2/DocumentSessionExtensions.cs
+++ Snittlistan.Web/Areas/V2/DocumentSessionExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using Raven.Abstractions;
 using Raven.Client;
+using Snittlistan.Web.Areas.V2.Domain;
 using Snittlistan.Web.Areas.V2.Indexes;
 
 namespace Snittlistan.Web.Areas.V2
@@ -10,23 +12,22 @@ namespace Snittlistan.Web.Areas.V2
     {
         public static List<SelectListItem> CreateRosterSelectList(this IDocumentSession session, int season, string rosterId = "")
         {
-            return session.Query<RosterSearchTerms.Result, RosterSearchTerms>()
-                .Where(x => x.Season == season)
-                .Where(x => x.Preliminary == false)
-                .Where(x => x.PlayerCount > 0)
-                .Where(x => x.BitsMatchId != 0)
-                .OrderBy(x => x.Date)
-                .ProjectFromIndexFieldsInto<RosterSearchTerms.Result>()
-                .ToList()
-                .Where(x => x.MatchResultId == null || string.IsNullOrEmpty(rosterId) == false)
-                .Select(
-                    x => new SelectListItem
-                    {
-                        Text = $"{x.Turn}: {x.Team} - {x.Opponent} ({x.Location} {x.Date.ToShortTimeString()})",
-                        Value = x.Id,
-                        Selected = x.Id == rosterId
-                    })
-                .ToList();
+            return session.Query<Roster, RosterSearchTerms>()
+                          .Where(x => x.Season == season)
+                          .Where(x => x.Preliminary == false)
+                          .Where(x => x.BitsMatchId != 0)
+                          .Where(x => x.Date < SystemTime.UtcNow)
+                          .OrderBy(x => x.Date)
+                          .ToList()
+                          .Where(x => x.MatchResultId == null || string.IsNullOrEmpty(rosterId) == false)
+                          .Select(
+                              x => new SelectListItem
+                              {
+                                  Text = $"{x.Turn}: {x.Team} - {x.Opponent} ({x.Location} {x.Date.ToShortTimeString()})",
+                                  Value = x.Id,
+                                  Selected = x.Id == rosterId
+                              })
+                          .ToList();
         }
     }
 }
