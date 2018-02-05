0319a040ce395eb2d28ee0efe66d640a02b717c1
Daniel Lidstrom <dlidstrom@gmail.com>
2018-02-05 06:37:27 +0100

    Made more generic

--- Snittlistan.Tool/PageAutomation/getMatches.js
+++ Snittlistan.Tool/PageAutomation/getMatches.js
@@ -7,14 +7,17 @@ var sys = require('system');
 var webpage = require('webpage');
 var fs = require('fs');
 
-if (sys.args.length != 4) {
-    console.log('Usage:', sys.args[0], 'TeamGeneralName TeamSpecificName output-file')
+if (sys.args.length != 5) {
+    console.log('Usage:', sys.args[0], 'TeamGeneralName TeamSpecificName standings-file match-scheme-file')
     phantom.exit();
 }
 
 var teamGeneralName = sys.args[1];
 var teamSpecificName = sys.args[2];
-var outputFile = sys.args[3];
+var standingsFile = sys.args[3];
+var matchSchemeFile = sys.args[3];
+
+console.log(teamGeneralName, teamSpecificName, standingsFile, matchSchemeFile);
 
 // start
 var page = webpage.create();
@@ -68,14 +71,14 @@ var steps = [
         click('MainContentPlaceHolder_Standings1_ButtonSearchClub');
     },
     function() {
-        page.evaluate(function() {
-            console.log('looking for team');
+        console.log('looking for', teamSpecificName);
+        page.evaluate(function(teamSpecificName) {
             var options = document.querySelectorAll('#MainContentPlaceHolder_Standings1_DropDownListTeams option');
             console.log('options.length', options.length);
             for (var i = 0; i < options.length; i++) {
                 var option = options[i];
                 console.log('option', i, option.text);
-                if (option.text === 'Fredrikshof IF BK A') {
+                if (option.text === teamSpecificName) {
                     console.log('found team');
                     var sel = document.getElementById('MainContentPlaceHolder_Standings1_DropDownListTeams');
                     sel.selectedIndex = i;
@@ -85,39 +88,35 @@ var steps = [
                     break;
                 }
             }
-        });
+        }, teamSpecificName);
     },
     function() {
-        page.render('page.png');
+        page.render('standings.png');
     },
     function() {
-        var team = page.evaluate(function() {
-            // time to extract contents
-
-            // direct link to page
-            var directLinkElement = document.getElementById('MainContentPlaceHolder_Standings1_LabelDirectLink');
-
-            // table standings
-            var tableStandingsRowElements = document.querySelectorAll('#MainContentPlaceHolder_Standings1_TableStandings tr');
-
-            var standingsLink = directLinkElement ? directLinkElement.textContent : '';
-            var team = {
-                standingsLink: standingsLink
-            };
-            var teamJson = JSON.stringify(team, null, 2);
-            console.log(teamJson);
-            return teamJson;
-        });
-
-        fs.write("team.json", team);
-        fs.write("page.html", page.content);
+        fs.write(standingsFile, page.content);
+    },
+    function() {
+        console.log('clicking MatchScheme');
+        click('MainContentPlaceHolder_Standings1_HyperLinkDivision');
+    },
+    function() {
+        page.render('matchScheme.png');
+    },
+    function() {
+        fs.write(matchSchemeFile, page.content);
     }
 ];
 
 var interval = setInterval(function() {
     if (!loadInProgress && typeof steps[testindex] == "function") {
         console.log("step " + (testindex + 1));
-        steps[testindex]();
+        try {
+            steps[testindex]();
+        } catch (e) {
+            console.error(e)
+        }
+
         testindex++;
     }
     if (typeof steps[testindex] != "function") {
