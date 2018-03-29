fa8818fc49f739325ac3ec924b2a0966c8a4da6b
Daniel Lidstrom <dlidstrom@gmail.com>
2018-03-29 06:09:23 +0200

    Pdf output that I'm going to discard in favour of a better solution

---
+++ Snittlistan.Web/Areas/V2/Controllers/EliteMedalsPrintController.cs
@@ -0,0 +1,47 @@
+﻿namespace Snittlistan.Web.Areas.V2.Controllers
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
+    using Domain;
+    using Helpers;
+    using Raven.Abstractions;
+    using Rotativa;
+    using Rotativa.Options;
+    using Web.Controllers;
+
+    [Authorize]
+    public class EliteMedalsPrintController : AbstractController
+    {
+        [HttpPost]
+        public ActionResult GeneratePdf(PostModel postModel)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return RedirectToAction("EliteMedals", "MatchResult");
+            }
+
+            // find out current season
+            var season = DocumentSession.LatestSeasonOrDefault(SystemTime.UtcNow.Year);
+            var eliteMedals = DocumentSession.Load<EliteMedals>(EliteMedals.TheId);
+
+            var filename = $"Elitmedaljer_{season}-{season+1}.pdf";
+            var viewModel = new EliteMedalsPrintViewModel();
+            //return View(viewModel);
+            return new ViewAsPdf(viewModel)
+            {
+                PageSize = Size.A4
+                //, FileName = filename
+            };
+        }
+
+        public class PostModel
+        {
+            [Required]
+            public string Name { get; set; }
+        }
+
+        public class EliteMedalsPrintViewModel
+        {
+        }
+    }
+}
