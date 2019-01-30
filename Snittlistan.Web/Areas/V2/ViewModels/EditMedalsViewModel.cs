1ea9f79f7c256960375277032a26de46a4ea522c
Daniel Lidstrom <dlidstrom@gmail.com>
2019-01-30 21:49:28 +0100

    Fixes #412

--- Snittlistan.Web/Areas/V2/ViewModels/EditMedalsViewModel.cs
+++ Snittlistan.Web/Areas/V2/ViewModels/EditMedalsViewModel.cs
@@ -4,17 +4,24 @@ namespace Snittlistan.Web.Areas.V2.ViewModels
 {
     public class EditMedalsViewModel
     {
-        public EditMedalsViewModel(string name, EliteMedals.EliteMedal.EliteMedalValue existingMedal, int capturedSeason)
+        public EditMedalsViewModel(
+            string name,
+            EliteMedals.EliteMedal.EliteMedalValue existingMedal,
+            int capturedSeason,
+            int currentSeason)
         {
             Name = name;
             ExistingMedal = existingMedal;
             CapturedSeason = capturedSeason;
+            CurrentSeason = currentSeason;
         }
 
-        public string Name { get; private set; }
+        public string Name { get; }
 
-        public EliteMedals.EliteMedal.EliteMedalValue ExistingMedal { get; private set; }
+        public EliteMedals.EliteMedal.EliteMedalValue ExistingMedal { get; }
 
-        public int CapturedSeason { get; private set; }
+        public int CapturedSeason { get; }
+
+        public int CurrentSeason { get; }
     }
 }
