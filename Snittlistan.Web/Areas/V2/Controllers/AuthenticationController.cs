9b010f0fb4fdd3da4aadd020a8d5a18a14ea4647
Daniel Lidstrom <dlidstrom@gmail.com>
2019-02-06 23:31:03 +0100

    Split the views

--- Snittlistan.Web/Areas/V2/Controllers/AuthenticationController.cs
+++ Snittlistan.Web/Areas/V2/Controllers/AuthenticationController.cs
@@ -1,13 +1,16 @@
-﻿using System;
-using System.Diagnostics;
-using System.Web.Mvc;
-using Snittlistan.Web.Areas.V2.ViewModels;
-using Snittlistan.Web.Controllers;
-using Snittlistan.Web.Helpers;
-using Snittlistan.Web.Services;
-
-namespace Snittlistan.Web.Areas.V2.Controllers
+﻿namespace Snittlistan.Web.Areas.V2.Controllers
 {
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Web.Mvc;
+    using Domain;
+    using Indexes;
+    using Snittlistan.Web.Controllers;
+    using Snittlistan.Web.Helpers;
+    using Snittlistan.Web.Services;
+
     public class AuthenticationController : AbstractController
     {
         private readonly IAuthenticationService authenticationService;
@@ -17,22 +20,51 @@ namespace Snittlistan.Web.Areas.V2.Controllers
             this.authenticationService = authenticationService ?? throw new ArgumentNullException(nameof(authenticationService));
         }
 
-        public ActionResult LogOn()
+        public ActionResult LogOnEmail()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult LogOn(LogOnViewModel vm, string returnUrl)
+        public ActionResult LogOnEmail(EmailViewModel vm, string returnUrl)
         {
             // find the user in question
             var user = DocumentSession.FindUserByEmail(vm.Email);
 
             if (user == null)
             {
-                ModelState.AddModelError("Email", "Användaren existerar inte.");
+                // locate player
+                var player = DocumentSession.Query<Player, PlayerSearch>().FirstOrDefault(x => x.Email == vm.Email);
+                if (player == null)
+                {
+                    ModelState.AddModelError("Email", "Användaren existerar inte.");
+                }
+                else
+                {
+                    // TODO: send the email here
+                    return View("EmailSent");
+                }
             }
-            else if (!user.ValidatePassword(vm.Password))
+
+            // redisplay form if any errors at this point
+            if (!ModelState.IsValid)
+                return View(vm);
+
+            return RedirectToAction("LogOnPassword", new { vm.Email, returnUrl });
+        }
+
+        public ActionResult LogOnPassword(string email, string returnUrl)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogOnPassword(string email, string returnUrl, PasswordViewModel vm)
+        {
+            // find the user in question
+            var user = DocumentSession.FindUserByEmail(email);
+
+            if (!user.ValidatePassword(vm.Password))
             {
                 ModelState.AddModelError("Password", "Lösenordet stämmer inte!");
             }
@@ -64,6 +96,22 @@ namespace Snittlistan.Web.Areas.V2.Controllers
         {
             authenticationService.SignOut();
             return RedirectToAction("Index", "Roster");
+        }
+
+        public class EmailViewModel
+        {
+            [Required(ErrorMessage = "Ange e-postadress")]
+            [DataType(DataType.EmailAddress), Display(Name = "E-postadress")]
+            public string Email { get; set; }
+        }
+
+        public class PasswordViewModel
+        {
+            [Required(ErrorMessage = "Ange lösenord")]
+            [DataType(DataType.Password), Display(Name = "Lösenord")]
+            public string Password { get; set; }
+
+            public bool RememberMe { get; set; }
         }
     }
 }
