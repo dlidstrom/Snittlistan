fd5df60deec2c1759b29ac8176fb9fecb12ce61e
Daniel Lidstrom <dlidstrom@gmail.com>
2019-02-12 15:05:38 +0100

    Disable roster info for now

--- Snittlistan.Web/Areas/V2/ViewModels/RosterCalendarEvent.cs
+++ Snittlistan.Web/Areas/V2/ViewModels/RosterCalendarEvent.cs
@@ -1,14 +1,14 @@
-using System;
-using Snittlistan.Web.Areas.V2.Domain;
-using Snittlistan.Web.Areas.V2.ReadModels;
-
 namespace Snittlistan.Web.Areas.V2.ViewModels
 {
+    using System;
+    using Snittlistan.Web.Areas.V2.Domain;
+    using Snittlistan.Web.Areas.V2.ReadModels;
+
     public class RosterCalendarEvent
     {
         private const string TextLineFeed = @"\" + "n";
 
-        public RosterCalendarEvent(Roster roster, Player[] players, Player teamLeader, ResultHeaderReadModel resultHeaderReadModel)
+        public RosterCalendarEvent(Roster roster, ResultHeaderReadModel resultHeaderReadModel)
         {
             Id = roster.Id;
             Date = roster.Date;
@@ -16,11 +16,7 @@ namespace Snittlistan.Web.Areas.V2.ViewModels
             Opponent = roster.Opponent;
             Location = roster.Location;
             Description = string.Empty;
-            if (resultHeaderReadModel == null)
-            {
-                GetPlayersDescription(roster, players, teamLeader);
-            }
-            else
+            if (resultHeaderReadModel != null)
             {
                 Description = resultHeaderReadModel.MatchCommentary
                     + TextLineFeed
@@ -29,44 +25,11 @@ namespace Snittlistan.Web.Areas.V2.ViewModels
             }
         }
 
-        private void GetPlayersDescription(Roster roster, Player[] players, Player teamLeader)
-        {
-            if (roster.Preliminary) return;
-
-            if (players.Length >= 8)
-            {
-                Description = "1 " + players[0].Nickname + " " + players[1].Nickname
-                              + TextLineFeed + "2 " + players[2].Nickname + " " + players[3].Nickname
-                              + TextLineFeed + "3 " + players[4].Nickname + " " + players[5].Nickname
-                              + TextLineFeed + "4 " + players[6].Nickname + " " + players[7].Nickname;
-                if (players.Length >= 9)
-                {
-                    Description += TextLineFeed + "R " + players[8].Nickname;
-                    if (players.Length >= 10)
-                    {
-                        Description += TextLineFeed + "R " + players[9].Nickname;
-                    }
-                }
-            }
-            else if (players.Length == 4)
-            {
-                Description = "1 " + players[0].Nickname
-                                   + TextLineFeed + "2 " + players[1].Nickname
-                                   + TextLineFeed + "3 " + players[2].Nickname
-                                   + TextLineFeed + "4 " + players[3].Nickname;
-            }
-
-            if (teamLeader != null)
-            {
-                Description += TextLineFeed + "LL " + teamLeader.Nickname;
-            }
-        }
-
-        public string Id { get; private set; }
-        public DateTime Date { get; private set; }
-        public string Team { get; private set; }
-        public string Opponent { get; private set; }
-        public string Location { get; private set; }
-        public string Description { get; private set; }
+        public string Id { get; }
+        public DateTime Date { get; }
+        public string Team { get; }
+        public string Opponent { get; }
+        public string Location { get; }
+        public string Description { get; }
     }
 }
