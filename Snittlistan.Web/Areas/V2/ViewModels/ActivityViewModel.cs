e320d63fbe0814a49b17b0c6a0ecb47c74686ae3
Daniel Lidstrom <dlidstrom@gmail.com>
2019-03-06 22:15:11 +0100

    Can view activities and also comment with Disqus

---
+++ Snittlistan.Web/Areas/V2/ViewModels/ActivityViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Snittlistan.Web.Areas.V2.ViewModels
+{
+    using System;
+    using Domain;
+
+    public class ActivityViewModel
+    {
+        public ActivityViewModel(Activity activity)
+        {
+            Id = activity.Id;
+            Title = activity.Title;
+            ActivityDate = activity.Date;
+            Message = activity.Message;
+        }
+
+        public string Id { get; }
+
+        public string Title { get; }
+
+        public DateTime ActivityDate { get; }
+
+        public string Message { get; }
+    }
+}
