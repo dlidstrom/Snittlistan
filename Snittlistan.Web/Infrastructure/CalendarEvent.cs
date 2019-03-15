b26bd5f05924d198a76f55e79d6dbbfb9e10a2e2
Daniel Lidstrom <dlidstrom@gmail.com>
2019-03-15 17:44:15 +0100

    Fixes #430

--- Snittlistan.Web/Infrastructure/CalendarEvent.cs
+++ Snittlistan.Web/Infrastructure/CalendarEvent.cs
@@ -1,6 +1,7 @@
 namespace Snittlistan.Web.Infrastructure
 {
     using System;
+    using System.Collections.Generic;
     using System.IO;
 
     public abstract class CalendarEvent
@@ -11,21 +12,53 @@ namespace Snittlistan.Web.Infrastructure
 
         protected static void WriteWithLineFeeds(TextWriter writer, string desc)
         {
-            var escaped = desc.Replace("\n", TextLineFeed);
-            WriteNonEmptyLine(writer, escaped.Substring(0, Math.Min(escaped.Length, 74)));
-            while (escaped.Length > 74)
+            writer.Write(FoldLines(Escape(desc)));
+        }
+
+        private static string Escape(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
             {
-                escaped = escaped.Substring(74);
-                WriteNonEmptyLine(writer, " " + escaped.Substring(0, Math.Min(escaped.Length, 74)));
+                return value;
+            }
+
+            value = value.Replace(SerializationConstants.LineBreak, @"\n");
+            value = value.Replace("\r", @"\n");
+            value = value.Replace("\n", @"\n");
+            value = value.Replace(";", @"\;");
+            value = value.Replace(",", @"\,");
+            return value;
+        }
+
+        private static string FoldLines(string incoming)
+        {
+            // The spec says nothing about trimming, but it seems reasonable...
+            var trimmed = incoming.Trim();
+            if (trimmed.Length <= 75)
+            {
+                return trimmed + SerializationConstants.LineBreak;
+            }
+
+            const int TakeLimit = 74;
+
+            var firstLine = trimmed.Substring(0, TakeLimit);
+            var remainder = trimmed.Substring(TakeLimit, trimmed.Length - TakeLimit);
+
+            var chunkedRemainder = string.Join(SerializationConstants.LineBreak + " ", Chunk(remainder));
+            return firstLine + SerializationConstants.LineBreak + " " + chunkedRemainder + SerializationConstants.LineBreak;
+        }
+
+        private static IEnumerable<string> Chunk(string str, int chunkSize = 73)
+        {
+            for (var index = 0; index < str.Length; index += chunkSize)
+            {
+                yield return str.Substring(index, Math.Min(chunkSize, str.Length - index));
             }
         }
 
-        private static void WriteNonEmptyLine(TextWriter writer, string line)
+        private static class SerializationConstants
         {
-            if (string.IsNullOrWhiteSpace(line) == false)
-            {
-                writer.WriteLine(line);
-            }
+            public const string LineBreak = "\r\n";
         }
     }
 }
