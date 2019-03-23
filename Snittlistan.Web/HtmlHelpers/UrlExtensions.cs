e90b8f94b75d42f34a6460950ab3cfaa97be62a7
Daniel Lidstrom <dlidstrom@gmail.com>
2019-03-24 00:21:44 +0100

    Fixed bundling and minification (don't do it)

--- Snittlistan.Web/HtmlHelpers/UrlExtensions.cs
+++ Snittlistan.Web/HtmlHelpers/UrlExtensions.cs
@@ -2,16 +2,36 @@ namespace Snittlistan.Web.HtmlHelpers
 {
     using System;
     using System.Diagnostics;
+    using System.IO;
+    using System.Security.Cryptography;
+    using System.Text;
+    using System.Web.Hosting;
     using System.Web.Mvc;
 
     public static class UrlExtensions
     {
+        private static readonly MD5 HashAlgorithm = MD5.Create();
+
         public static string ContentCacheBreak(this UrlHelper url, string contentPath)
         {
             if (contentPath == null) throw new ArgumentNullException(nameof(contentPath));
-            var path = url.Content(contentPath);
-            path += "?" + MvcApplication.GetAssemblyVersion();
-            return path;
+            var path = HostingEnvironment.MapPath(contentPath);
+            if (File.Exists(path) == false) throw new Exception($"{path} does not exist");
+            var hashPart = string.Empty;
+            if (path != null)
+            {
+                var bytes = File.ReadAllBytes(path);
+                var hash = HashAlgorithm.ComputeHash(bytes);
+                var hashBuilder = new StringBuilder();
+                foreach (var b in hash)
+                {
+                    hashBuilder.Append($"{b:x2}");
+                }
+
+                hashPart = $"?{hashBuilder}";
+            }
+
+            return $"{url.Content(contentPath)}{hashPart}";
         }
 
         public static string GetWebcalUrl(this UrlHelper helper)
