bb78c0d2a960b25cc078e603d1630c2a606943a8
Daniel Lidstrom <dlidstrom@gmail.com>
2020-04-07 16:36:42 +0200

    Improved logging

--- Snittlistan.Web/Infrastructure/Installers/BitsClientInstaller.cs
+++ Snittlistan.Web/Infrastructure/Installers/BitsClientInstaller.cs
@@ -6,12 +6,16 @@ namespace Snittlistan.Web.Infrastructure.Installers
     using Castle.MicroKernel.Registration;
     using Castle.MicroKernel.SubSystems.Configuration;
     using Castle.Windsor;
+    using NLog;
 
     public class BitsClientInstaller : IWindsorInstaller
     {
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+
         public void Install(IWindsorContainer container, IConfigurationStore store)
         {
             var apiKey = Environment.GetEnvironmentVariable("ApiKey");
+            Logger.Info("ApiKey: {0}", apiKey);
             container.Register(Component.For<IBitsClient>().Instance(new BitsClient(apiKey, new HttpClient())));
         }
     }
