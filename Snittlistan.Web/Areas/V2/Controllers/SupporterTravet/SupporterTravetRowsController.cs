8c50d701a7e8fb42d9986577c061724a7be40075
Daniel Lidstrom <dlidstrom@gmail.com>
2020-09-28 10:06:26 +0200

    another page

---
+++ Snittlistan.Web/Areas/V2/Controllers/SupporterTravet/SupporterTravetRowsController.cs
@@ -0,0 +1,13 @@
+﻿namespace Snittlistan.Web.Areas.V2.Controllers.SupporterTravet
+{
+    using System.Web.Mvc;
+    using Snittlistan.Web.Controllers;
+
+    public class SupporterTravetRowsController : AbstractController
+    {
+        public ActionResult Index()
+        {
+            return View(new SupporterTravetModels.Header("sp-123"));
+        }
+    }
+}
