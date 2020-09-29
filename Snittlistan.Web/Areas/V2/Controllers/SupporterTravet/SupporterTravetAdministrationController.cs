6e9ce8d63b1e5df792f69bea9ec902b8ae26b928
Daniel Lidstrom <dlidstrom@gmail.com>
2020-09-29 12:00:30 +0200

    administration

---
+++ Snittlistan.Web/Areas/V2/Controllers/SupporterTravet/SupporterTravetAdministrationController.cs
@@ -0,0 +1,13 @@
+﻿namespace Snittlistan.Web.Areas.V2.Controllers.SupporterTravet
+{
+    using System.Web.Mvc;
+    using Snittlistan.Web.Controllers;
+
+    public class SupporterTravetAdministrationController : AbstractController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
