35e7c0b4d00bb393d2c400d43ecb235c4ee0d536
Daniel Lidstrom <dlidstrom@gmail.com>
2020-10-21 14:22:28 +0200

    preparations

---
+++ SportBowling/Cms/Cms.Theme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "Cms.Theme",
+    Author = "The Orchard Core Team",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1",
+    Description = "Cms.Theme"
+)]
