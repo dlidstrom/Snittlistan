918626e00395887dd7033a5c0f18c3177c560c4f
Daniel Lidstrom <dlidstrom@gmail.com>
2021-10-18 10:53:54 +0200

    fix mail

--- Snittlistan.Web/Models/InviteUserEmail.cs
+++ Snittlistan.Web/Models/InviteUserEmail.cs
@@ -4,8 +4,8 @@ namespace Snittlistan.Web.Models
 {
     public class InviteUserEmail : EmailBase
     {
-        public InviteUserEmail(string recipient, string subject, string activationUri)
-            : base("InviteUser", recipient, subject)
+        public InviteUserEmail(string to, string subject, string activationUri)
+            : base("InviteUser", to, subject)
         {
             ActivationUri = activationUri;
         }
