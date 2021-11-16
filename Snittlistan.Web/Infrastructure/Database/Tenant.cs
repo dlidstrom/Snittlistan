5f29618e5389f55e597dabe6f11022e68efc71da
Daniel Lidstrom <dlidstrom@gmail.com>
2021-11-16 18:57:50 +0100

    working on tests and in-memory context

---
+++ Snittlistan.Web/Infrastructure/Database/Tenant.cs
@@ -0,0 +1,50 @@
+﻿#nullable enable
+
+namespace Snittlistan.Web.Infrastructure.Database
+{
+    using System;
+
+    public class Tenant
+    {
+        public Tenant(
+            int tenantId,
+            string hostname,
+            string favicon,
+            string appleTouchIcon,
+            string appleTouchIconSize,
+            string webAppTitle,
+            int clubId)
+        {
+            TenantId = tenantId;
+            ClubId = clubId;
+            Hostname = hostname;
+            Favicon = favicon;
+            AppleTouchIcon = appleTouchIcon;
+            AppleTouchIconSize = appleTouchIconSize;
+            WebAppTitle = webAppTitle;
+            CreatedDate = DateTime.Now;
+        }
+
+        private Tenant()
+        {
+        }
+
+        public int TenantId { get; set; }
+
+        public int ClubId { get; }
+
+        public string Hostname { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string Favicon { get; set; } = null!;
+
+        public string AppleTouchIcon { get; set; } = null!;
+
+        public string AppleTouchIconSize { get; set; } = null!;
+
+        public string WebAppTitle { get; set; } = null!;
+
+        public DateTime CreatedDate { get; private set; }
+    }
+}
