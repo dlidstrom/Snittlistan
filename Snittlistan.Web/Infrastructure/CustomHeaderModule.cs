86a8ac2e4a467580aa06e2a67ea106494cf79464
Daniel Lidstrom <dlidstrom@gmail.com>
2022-01-28 19:30:03 +0100

    remove http headers

---
+++ Snittlistan.Web/Infrastructure/CustomHeaderModule.cs
@@ -0,0 +1,21 @@
+﻿#nullable enable
+
+using System.Web;
+
+namespace Snittlistan.Web.Infrastructure;
+
+public class CustomHeaderModule : IHttpModule
+{
+    public void Init(HttpApplication context)
+    {
+        context.PreSendRequestHeaders += OnPreSendRequestHeaders;
+    }
+
+    public void Dispose() { }
+
+    private void OnPreSendRequestHeaders(object sender, EventArgs e)
+    {
+        // removes "Server" details from response header
+        HttpContext.Current.Response.Headers.Remove("Server");
+    }
+}
