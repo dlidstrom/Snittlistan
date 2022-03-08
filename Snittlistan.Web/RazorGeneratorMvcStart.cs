82121f3e3b9a825fbe733393f3fe1e65f9ecf8fa
Daniel Lidstrom <dlidstrom@gmail.com>
2022-03-08 12:44:53 +0100

    new view engine

---
+++ Snittlistan.Web/RazorGeneratorMvcStart.cs
@@ -0,0 +1,28 @@
+﻿#nullable enable
+
+using RazorGenerator.Mvc;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages;
+
+[assembly: WebActivatorEx.PostApplicationStartMethod(
+    typeof(Snittlistan.Web.RazorGeneratorMvcStart),
+    "Start")]
+
+namespace Snittlistan.Web;
+
+public static class RazorGeneratorMvcStart
+{
+    public static void Start()
+    {
+        PrecompiledMvcEngine engine = new(typeof(RazorGeneratorMvcStart).Assembly)
+        {
+            UsePhysicalViewsIfNewer = HttpContext.Current.Request.IsLocal
+        };
+
+        ViewEngines.Engines.Insert(0, engine);
+
+        // StartPage lookups are done by WebPages.
+        VirtualPathFactoryManager.RegisterVirtualPathFactory(engine);
+    }
+}
